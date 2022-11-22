debfdd9bcf2cfe26137df575bc81ea622b81dbf4
Jooty <compundbread@gmail.com>
2022-11-22 14:33:44 -0500

    feat: Basic KI packet deserialization.

---
+++ src/Engine/KIPacket.cs
@@ -0,0 +1,34 @@
+﻿using Imlight.Common.Logger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imlight.Engine
+{
+    /// <summary>
+    /// A packet containing KI protocol message framing.
+    /// </summary>
+    public class KIPacket
+    {
+
+        internal class Header
+        {
+            internal bool isLarge;
+            internal UInt16 length;
+            internal UInt32 bigLength;
+
+            internal UInt32 Length()
+            {
+                if (this.isLarge) return this.bigLength;
+                else return this.length;
+            }
+        }
+        internal Header PacketHeader { get; set; }
+
+
+    }
+}
