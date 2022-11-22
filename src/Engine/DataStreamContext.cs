c1935849c318e3a5dffa5dc5044119f3f2b3357a
Jooty <compundbread@gmail.com>
2022-11-22 14:36:01 -0500

    feat: Add engine workload pool.
    + Add DataStreamContext, which contains basic information about data and it's corresponding socket.
    + Add WizardMessageContext, which serves as context for once we know a DataStreamContext contains a KI packet.

---
+++ src/Engine/DataStreamContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imlight.Engine
+{
+    public class DataStreamContext
+    {
+
+        public NetworkStream Stream;
+        public sbyte RealmID;
+        public short SocketID;
+
+        public DataStreamContext(NetworkStream stream, sbyte realmID, short socketID)
+        {
+            Stream = stream;
+            RealmID = realmID;
+            SocketID = socketID;
+        }
+    }
+}
