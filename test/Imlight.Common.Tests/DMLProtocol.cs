d3e125a48868adc41bafa4439f5c42b6dec5555b
Jooty <compundbread@gmail.com>
2022-12-18 06:41:23 -0500

    test: Introduce dynamic DML testing unit.

---
+++ test/Imlight.Common.Tests/DMLProtocol.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using Imlight.Common.Logger;
+
+namespace Imlight.Engine.DML
+{
+    public class DMLProtocol
+    {
+
+        public class DMLProtocolInfo
+        {
+            internal readonly byte ServiceID;
+            internal readonly string ProtocolType;
+            internal readonly Int32 ProtocolVersion;
+            internal readonly string ProtocolDescription;
+
+            internal readonly string Name;
+
+            public DMLProtocolInfo(byte serviceID, string protocolType, int protocolVersion, string protocolDescription)
+            {
+                ServiceID = serviceID;
+                ProtocolType = protocolType;
+                ProtocolVersion = protocolVersion;
+                ProtocolDescription = protocolDescription;
+
+                // Create a formmated name for logging.
+                Name = $"{this.ProtocolType}_{this.ServiceID}_PROTOCOL";
+            }
+        }
+        public DMLProtocolInfo Info { get; private set; }
+        public Dictionary<byte, DMLRecord> Records { get; private set; }
+        public string Name { get { return this.Info?.Name; } }
+
+        /// <summary>
+        /// Sets this object's data from an XML file path.
+        /// </summary>
+        /// <param name="recordFilePath">The local file path to the DML protocol XML file.</param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="Exception"></exception>
+        public void FromXMLTemplate(string recordFilePath)
+        {
+            // Read file contents as XML from the parameter.
+            XmlDocument xmlDoc;
+            if (!TryGetProtocolXml(recordFilePath, out xmlDoc))
+                throw new ArgumentException($"No xml file found at \"{recordFilePath}\"!");
+
+            // Attempt to set the DMLProtocolInfo for this Protocol.
+            if (TryGetProtocolInfo(xmlDoc, out var info))
+            {
+                this.Info = info;
+                Log.Verbose($"DMLProtocol \"{this.Name}\" set information.");
+            }
+            else throw new ArgumentException($"DMLProtocolInfo could not be set for given path \"{recordFilePath}\"!");
+
+            // Attempt to set each DMLRecord for this protocol using the given XML document.
+            if (TryGetProtocolRecords(xmlDoc, out var records))
+            {
+                this.Records = records;
+                Log.Verbose($"DMLProtocol \"{this.Name}\" set all records.");
+            }
+            else throw new Exception($"DMLProtocol \"{this.Name}\" could not set records.");
+        }
+
+        /// <summary>
+        /// Returns a DML Record from a record, and fill the elements data with a byte array. Useful for creating DML records from network packets.
+        /// </summary>
+        /// <param name="ID">The ID of the DML record.</param>
+        /// <param name="rawBytes">The raw byte array to parse as the DML elements of the record.</param>
+        /// <returns>The DML record, with all elements filled out.</returns>
+        public DMLRecord CreateDMLRecordFromBinary(byte ID, byte[] rawBytes)
+        {
+            if (!TryCloneRecordTemplate(ID, out DMLRecord record))
+                throw new ArgumentException($"Could not find a DML record with id [{ID}]!");
+
+            record.FromBinary(rawBytes);
+
+            return record;
+        }
+
+        /// <summary>
+        /// Attempts to clone a record by ID.
+        /// </summary>
+        /// <param name="ID">The ID of the record to search for.</param>
+        /// <param name="record">The outgoing record, if one exists.</param>
+        /// <returns>True, if a record is found. False otherwise.</returns>gh
+        public bool TryCloneRecordTemplate(byte ID, out DMLRecord record)
+        {
+            record = default;
+            if (this.Records.TryGetValue(ID, out var val))
+            {
+                record = (DMLRecord)val.Clone();
+                return true;
+            }
+            else return false;
+        }
+
+        /// <summary>
+        /// Attempts to parse the record data of a *Messages.xml file from a path.
+        /// </summary>
+        /// <param name="path">The path to the XML file.</param>
+        /// <param name="document">The returning XMLDocument object.</param>
+        /// <returns>True, if succeeded. False otherwise.</returns>
+        private bool TryGetProtocolXml(string path, out XmlDocument document)
+        {
+            document = default;
+            if (!File.Exists(path)) return false;
+
+            // Log
+            Log.Debug($"Attempting to get XML data from \"{path}\" ... ");
+
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(path);
+
+                document = xmlDoc;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal($"Failed to parse XML data for \"{path}\" | Exception: {ex.Message}");
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Attempts to parse the _ProtocolInfo RECORD from the given XMLDocument.
+        /// </summary>
+        /// <param name="xmlDoc">The successfully parsed XMLDocument.</param>
+        /// <returns>True on success. False otherwise.</returns>
+        private bool TryGetProtocolInfo(XmlDocument xmlDoc, out DMLProtocolInfo info)
+        {
+            info = default;
+
+            // Create ProtocolInfo object based off the information from the _ProtocolInfo node.
+            // These fields can be converted ahead of time, since the ProtocolInfo field data types are not unique.
+            try
+            {
+                byte serviceID = Convert.ToByte(xmlDoc.DocumentElement.SelectSingleNode("//_ProtocolInfo/RECORD/ServiceID").InnerText);
+                string protocolType = xmlDoc.DocumentElement.SelectSingleNode("//_ProtocolInfo/RECORD/ProtocolType").InnerText;
+                Int32 protocolVersion = Convert.ToInt32(xmlDoc.DocumentElement.SelectSingleNode("//_ProtocolInfo/RECORD/ProtocolVersion").InnerText);
+                string protocolDescription = xmlDoc.DocumentElement.SelectSingleNode("//_ProtocolInfo/RECORD/ProtocolDescription").InnerText;
+
+                info = new DMLProtocolInfo(serviceID, protocolType, protocolVersion, protocolDescription);
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal($"Failed to parse DMLProtocolInfo from XML. | Exception: {ex.Message}");
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Attempts to parse each DML record from the given XMLDocument.
+        /// </summary>
+        /// <param name="xmlDoc">The XML document.</param>
+        /// <param name="records">The returning array of DMLRecord objects, parsed from the given XML document.</param>
+        /// <returns>True, if the DMLRecords are found and successfully parsed. False otherwise.</returns>
+        private bool TryGetProtocolRecords(XmlDocument xmlDoc, out Dictionary<byte, DMLRecord> records)
+        {
+            records = new Dictionary<byte, DMLRecord>();
+
+            // The first node is the XML version node.
+            XmlNode messagesNode = xmlDoc.ChildNodes[1];
+            XmlNodeList recordsList = messagesNode.ChildNodes;
+
+            // Some protocols don't list their records with a message ID.
+            // If this is the case, the message ID is the index of which the messages appear ordinally.
+            XmlNode[] recordsSorted = SortXmlMessagesOrdinally(recordsList);
+
+            if (recordsSorted.Count() <= 0) return false;
+
+            try
+            {
+                foreach (XmlNode node in recordsSorted)
+                {
+                    // Skip the _ProtocolInfo & comments.
+                    if (node.Name == "_ProtocolInfo" 
+                        || node.NodeType == XmlNodeType.Comment) continue;
+
+                    // The information is stored in a nested node labeled "RECORD".
+                    XmlNode recordRaw = node.ChildNodes[0];
+                    
+                    // Create new DMLRecord object and set all it's data.
+                    DMLRecord dmlRecord = new DMLRecord();
+                    bool dmlRecordCreationResult = dmlRecord.FromXml(recordRaw);
+
+                    // Get the protocol ID.
+                    var index = (byte)(Array.IndexOf(recordsSorted, node));
+
+                    // If it succeeded in creating, add it to the records list.
+                    // If we couldn't set the DMLRecord data, return false. No need to continue.
+                    if (dmlRecordCreationResult) records.Add(index, dmlRecord);
+                    else return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Sort a protocol's child RECORDs ordinally.
+        /// </summary>
+        /// <param name="messagesChildrenList"></param>
+        /// <returns>An array of ordinally sorted XML nodes.</returns>
+        private XmlNode[] SortXmlMessagesOrdinally(XmlNodeList messagesChildrenList)
+        {
+            /*
+             * This was a pita for me earlier, so I'm going to document it here:
+             * <MSG_PING>                                                 <---- THIS IS WHAT TO SORT BY.
+                 <RECORD>
+                   <_MsgType TYPE="UBYT" NOXFER="TRUE">1</_MsgType>  
+                   <_MsgName TYPE="STR" NOXFER="TRUE">MSG_PING</_MsgName> <---- THIS IS NOT THE SORT.
+                   <_MsgDescription TYPE="STR" NOXFER="TRUE">PING request.</_MsgDescription>
+                   <_MsgHandler TYPE="STR" NOXFER="TRUE">MSG_Ping</_MsgHandler>
+                   <_MsgAccessLvl TYPE="UBYT" NOXFER="TRUE">0</_MsgAccessLvl>
+                 </RECORD>
+               </MSG_PING>
+            */
+
+            // Sorting using this method will always leve _ProtocolInfo as the very last element.
+            // Create an empty array to inevitably fill with sorted messages.
+            XmlNode[] sortedNodes = new XmlNode[messagesChildrenList.Count];
+
+            // Copy everything to the new array.
+            // Array.Copy does NOT work here, as the XmlNode object is only accessable via the iterator.
+            // @todo: Looking for a better fix.
+            for (int i = 0; i < messagesChildrenList.Count; i++)
+            {
+                sortedNodes[i] = messagesChildrenList[i];
+            }
+
+            Array.Sort(sortedNodes, (x, y) => String.CompareOrdinal(x.Name, y.Name));
+            return sortedNodes;
+        }
+
+        public void DisposeRecords()
+        {
+            if (this.Records.Count <= 0) return;
+
+            foreach (DMLRecord record in this.Records.Values)
+            {
+                record.Dispose();
+            }
+        }
+
+    }
+}
---
+++ test/Imlight.Common.Tests/DMLRecord.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Imlight.Common.Logger;
+
+namespace Imlight.Engine.DML
+{
+    public class DMLRecord : IDisposable, ICloneable
+    {
+
+        public List<DMLElement> Elements { get; }
+
+        private bool _recordTemplateSet;
+
+        // ctor
+        public DMLRecord()
+        {
+            Elements = new List<DMLElement>();
+            this._recordTemplateSet = false;
+        }
+
+        /// <summary>
+        /// Attempts to set this object's template data from the XML record node.
+        /// </summary>
+        /// <param name="recordNode">The original XML record.</param>
+        /// <returns>True on success, false otherwise.</returns>
+        public bool FromXml(XmlNode recordNode)
+        {
+            var xmlElements = recordNode.ChildNodes.OfType<XmlElement>();
+
+            if (xmlElements.Count() <= 0) return false;
+
+            foreach (var element in xmlElements)
+            {
+                // Create a new DMLElement object and attempt to set it's data.
+                // On success, add it to the element list of this object.
+                // Return the result of the operation either way.
+
+                DMLElement dmlElement = new DMLElement();
+                bool result = dmlElement.FromXml(element);
+
+                if (result) Elements.Add(dmlElement);
+                else return false;
+            }
+
+            this._recordTemplateSet = true;
+            return true;
+        }
+
+        /// <summary>
+        /// Attempts to set this object's data from a byte array. This method should be called *after* FromXml, as it requires those nodes written prior.
+        /// </summary>
+        /// <param name="rawBytes">The array of raw bytes, usually from a packet.</param>
+        /// <returns>True on success, false otherwise.</returns>
+        public bool FromBinary(byte[] rawBytes)
+        {
+            if (rawBytes == null) throw new ArgumentNullException(nameof(rawBytes));
+            if (!this._recordTemplateSet) 
+                throw new Exception($"This DML record does not have it's template yet! Use {nameof(FromXml)} prior to calling this method.");
+
+            // Create BinaryReader
+            Stream stream = new MemoryStream(rawBytes);
+            BinaryReader reader = new BinaryReader(stream);
+
+            foreach (DMLElement element in this.Elements)
+            {
+                if (element.IsMetadata()) continue;
+                if (element.Value != null) continue;
+
+                switch (element.Type)
+                {
+                    case DMLElement.DataType.BYT:
+                        element.Value = reader.ReadSByte();
+                        break;
+                    case DMLElement.DataType.UBYT:
+                        element.Value = reader.ReadByte();
+                        break;
+                    case DMLElement.DataType.SHRT:
+                        element.Value = reader.ReadInt16();
+                        break;
+                    case DMLElement.DataType.USHRT:
+                        element.Value = reader.ReadUInt16();
+                        break;
+                    case DMLElement.DataType.INT:
+                        element.Value = reader.ReadInt32();
+                        break;
+                    case DMLElement.DataType.UINT:
+                        element.Value = reader.ReadUInt32();
+                        break;
+                    case DMLElement.DataType.WSTR:
+                    case DMLElement.DataType.STR:
+                        element.Value = reader.ReadString();
+                        break;
+                    case DMLElement.DataType.FLT:
+                        element.Value = reader.ReadSingle();
+                        break;
+                    case DMLElement.DataType.DBL:
+                        element.Value = reader.ReadDouble();
+                        break;
+                    case DMLElement.DataType.GID:
+                        element.Value = reader.ReadUInt64();
+                        break;
+                    case DMLElement.DataType.UNKNOWN:
+                        break;
+                    default:
+                        throw new Exception($"Could not deserialize type [{element.Type}] from binary!");
+                }
+            }
+
+            return true;
+        }
+
+        // dtor
+        ~DMLRecord()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            Elements.Clear();
+            GC.SuppressFinalize(this);
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
