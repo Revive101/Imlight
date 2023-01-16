cba4b0f0d78ee52d8fe3c65fba6968912382c4c4
Jooty <compundbread@gmail.com>
2023-01-15 19:52:36 -0500

    feat: dict getorcreate()

---
+++ src/Imlight.Common/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imlight.Common
+{
+    public static class ExtensionMethods
+    {
+
+        public static TValue GetOrCreate<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key)
+            where TValue : new()
+        {
+            if (!dict.TryGetValue(key, out TValue val))
+            {
+                val = new TValue();
+                dict.Add(key, val);
+            }
+
+            return val;
+        }
+
+    }
+}
