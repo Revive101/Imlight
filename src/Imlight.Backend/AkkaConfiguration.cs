ec6b59214185fc7316176f4b82c018fef6ad8c9f
Jooty <compundbread@gmail.com>
2023-03-08 03:54:17 -0500

    feat: add akka.net config and serilog

---
+++ src/Imlight.Backend/AkkaConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Akka;
+using Akka.Actor;
+using Akka.Configuration;
+using Imlight.Common;
+
+namespace Imlight.Backend
+{
+    internal static class AkkaConfiguration
+    {
+        internal static readonly string _configFileLocation = @$"{Directory.GetCurrentDirectory()}\config\akka.conf";
+
+        internal static bool CreateActorSystem(string name, out ActorSystem system)
+        {
+            system = null;
+
+            Log.Logger.Information("Getting Akka.NET configuration..");
+
+            if (!GetAkkaConfiguration(out var config))
+                return false;
+
+            system = ActorSystem.Create(name, config);
+            return true;
+        }
+
+        internal static bool GetAkkaConfiguration(out Config config)
+        {
+            config = default;
+
+            try
+            {
+                if (!File.Exists(_configFileLocation))
+                    return false;
+
+                var configContents = File.ReadAllText(_configFileLocation);
+
+                config = ConfigurationFactory.ParseString(configContents);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.Logger.Error(e.Message);
+                return false;
+            }
+        }
+    }
+}
--- src/Imlight.Backend/Program.cs
+++ src/Imlight.Backend/Program.cs
@@ -28,10 +28,21 @@ namespace Imlight.Backend
         {
             PrintTitle();
 
+            // @todo: change this to a resource manager
             CoreObjectFactory.SetRoot($"{Directory.GetCurrentDirectory()}/Input/Root.wad");
 
-            _imlightSystem = ActorSystem.Create(ACTOR_SYSTEM_NAME);
+            if (!AkkaConfiguration.CreateActorSystem(ACTOR_SYSTEM_NAME, out var system))
+            {
+                Log.Logger.Fatal($"Could not find Akka.NET config file!");
 
+                Console.ReadKey();
+                return;
+            }
+
+            _imlightSystem = system;
+
+            // Otherwise, we've had a successful Akka.NET initialization and we're ready to start
+            // the servers.
             StartLoginServer();
             StartGameServer();
 
@@ -76,45 +87,6 @@ namespace Imlight.Backend
             Log.Logger.Information(@"==============================================================================================");
             Log.Logger.Information($"Imlight v{VERSION} -- Developed and maintained by Wizard101Rewritten.");
             Log.Logger.Information(@"==============================================================================================");
-        }
-
-        private static void cktest()
-        {
-            ushort sid = 3258;
-            uint secs = 1617815695;
-            uint milli = 805;
-            var test = "+FO9W7DLYNuvLdwvnMaxtJrSD+/h7HHfpzSNKv6G4UomKKoy+uwknGbqrtz4KNHSIS6McowtSTXtQBwwq7bwSQ==";
-
-            var y = ClientKey.VerifyCK1("1", sid, secs, milli, test);
-        }
-
-        private static void rec1test()
-        {
-            ushort sid = 44761;
-            uint secs = 1675419571;
-            uint millis = 763;
-            var ck1 = "+FO9W7DLYNuvLdwvnMaxtJrSD+/h7HHfpzSNKv6G4UomKKoy+uwknGbqrtz4KNHSIS6McowtSTXtQBwwq7bwSQ==";
-            var rec1Input = new byte[]
-            {
-                0xab, 0xb3, 0xf6, 0xeb, 0x61, 0x20, 0x34, 0x54,
-                0x77, 0x3c, 0x09, 0x65, 0xeb, 0x9c, 0x67, 0x49,
-                0xa2, 0xae, 0xac, 0x97, 0xe3, 0x38, 0x79, 0xb4,
-                0x40, 0x68, 0xc3, 0x0e, 0xd3, 0xc4, 0xad, 0xee,
-                0x0a, 0xa4, 0x3c, 0xec, 0xb4, 0xa4, 0x7b, 0xb1,
-                0xeb, 0x36, 0x91, 0x9e, 0xed, 0x32, 0xc1, 0x1b,
-                0xc7, 0x44, 0x7f, 0xc6, 0x47, 0x31, 0x6c, 0x98,
-                0x2b, 0x6a, 0x9b, 0x27, 0x58, 0x0c, 0x1c, 0xab,
-                0x32, 0x84, 0x0b, 0x57, 0x41, 0x25, 0x60, 0xf4,
-                0xf3, 0xf5, 0x8c, 0x66, 0xc5, 0x55, 0xa0, 0xb2,
-                0x2b, 0x1d, 0x76, 0x47, 0x43, 0x07, 0x48, 0x4f,
-                0x65, 0xa4, 0x79, 0x5b, 0x50, 0xee, 0x97, 0x66,
-                0xcf, 0x1a, 0x76, 0xd8, 0xfb, 0xc5, 0x52, 
-            };
-
-            var enc = Rec1.Encode(sid, "1", ck1, secs, millis);
-
-            var dec = Rec1.Decode(rec1Input, sid, secs, millis);
-            var decStr = new WizUnraveler.ByteString(dec);
         }
     }
 }
