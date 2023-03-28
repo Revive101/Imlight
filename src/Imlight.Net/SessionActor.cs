e7415d73cb45c345a5f6fb3683aaf02cf28ba106
Jooty <compundbread@gmail.com>
2023-03-28 11:33:09 -0400

    perf: significant socket resource decrease

--- src/Imlight.Net/SessionActor.cs
+++ src/Imlight.Net/SessionActor.cs
@@ -21,6 +21,7 @@ namespace Imlight.Net
     public class SessionActor : ReceiveActor, IDisposable
     {
         private const int BUFFER_SIZE = 4096;
+        private const bool CLOSE_ON_SOCKET_EXCEPTION = true;
 
         public ushort SessionID                     { get; }
         public Socket Socket                        { get; }
@@ -34,7 +35,8 @@ namespace Imlight.Net
         
         private readonly IActorRef _actorFactoryRef;
         private readonly Dictionary<IActorRef, MessageService> _services;
-        private readonly SocketAsyncEventArgs _sendEventArgs = new SocketAsyncEventArgs();
+        private readonly SocketAsyncEventArgs _socketSendArgs = new SocketAsyncEventArgs();
+        private readonly SocketAsyncEventArgs _socketReceiveArgs = new SocketAsyncEventArgs();
         private readonly CancellationTokenSource _cts = new CancellationTokenSource();
         private bool _isSending;
         private bool _isDisposed;
@@ -53,6 +55,9 @@ namespace Imlight.Net
             this._actorFactoryRef = server.Ask<SERVER_100_PROTOCOL.MSG_ACTORFACTORYINFO>(query)
                 .Result
                 .Reference;
+
+            //_socketSendArgs.Completed += OnSendCompleted;
+            //_socketReceiveArgs.Completed += OnReceiveCompleted;
 
             ActorRef = Context.Self;
 
@@ -177,7 +182,7 @@ namespace Imlight.Net
             Socket?.Close();
             _cts.Cancel();
             
-            _sendEventArgs?.Dispose();
+            _socketSendArgs?.Dispose();
             _cts?.Dispose();
             Socket?.Dispose();
         }
@@ -219,36 +224,98 @@ namespace Imlight.Net
 
         private async Task ListenAndProcessAsync(IActorRef context)
         {
-            while (!_cts.Token.IsCancellationRequested)
+            var buffer = new byte[BUFFER_SIZE];
+            _socketReceiveArgs.SetBuffer(buffer, 0, BUFFER_SIZE);
+            _socketReceiveArgs.Completed += OnReceiveCompleted;
+
+            while (!_cts.Token.IsCancellationRequested && Socket.Connected)
             {
                 try
                 {
-                    var buffer = new byte[BUFFER_SIZE];
-                    var bytesReceived = await Socket.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
-
-                    if (bytesReceived <= 0) continue;
-
-                    var packet = GetPacketFromBuffer(buffer, bytesReceived);
-                    if (packet == null) continue;
-
-                    if (!SessionValid && packet.ServiceID != 0)
+                    if (!Socket.ReceiveAsync(_socketReceiveArgs))
                     {
-                        _preInitMessages.Add(packet);
-                        continue;
+                        OnReceiveCompleted(Socket, _socketReceiveArgs);
                     }
-
-                    HandlePacket(packet);
                 }
                 catch (SocketException ex)
                 {
                     //Log.Logger.Error($"SessionActor [{SessionID}] socket error: {ex.Message}");
-                    break;
+                    if (CLOSE_ON_SOCKET_EXCEPTION) break;
                 }
+
+                await Task.Delay(1000);
             }
 
             context.Tell("Close");
         }
-        
+
+        private void SendToSocket(INetworkMessage message)
+        {
+            if (!Socket.Connected)
+            {
+                Log.Logger.Error($"SessionActor [{SessionID}] cannot send message [{message.GetType()}] " +
+                                 $"send failure: " +
+                                 $"Socket is not connected!");
+                return;
+            }
+            if (_isSending)
+            {
+                Log.Logger.Error($"SessionActor [{SessionID}] send failure: " +
+                                 $"Asynchronous send operation already in progress.");
+                return;
+            }
+
+            var data = MessageSerializer.SerializeMessageBinary(message);
+            _isSending = true;
+            _socketSendArgs.SetBuffer(data, 0, data.Length);
+            _socketSendArgs.UserToken = Socket;
+            _socketSendArgs.Completed += OnSendCompleted;
+
+            var willRaiseEvent = Socket.SendAsync(_socketSendArgs);
+            if (!willRaiseEvent) OnSendCompleted(this, _socketSendArgs);
+
+            var scopedMessageName = message
+                .GetType()
+                .ToString()
+                .Split('.')[^1]
+                .Replace('+', '.');
+            Log.Logger.Debug($"SessionActor [{SessionID}] sent message [{scopedMessageName}]");
+        }
+
+        private void OnSendCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            _isSending = false;
+            if (e.SocketError != SocketError.Success)
+            {
+                Log.Logger.Error($"SessionActor [{SessionID}] send failure: {e.SocketError}");
+                return;
+            }
+        }
+
+        private void OnReceiveCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.SocketError != SocketError.Success)
+            {
+                Log.Logger.Error($"SessionActor receive error: {e.SocketError}");
+                return;
+            }
+            else if (e.BytesTransferred <= 0) return;
+
+            var buffer = e.Buffer;
+            var bytesReceived = e.BytesTransferred;
+
+            var packet = GetPacketFromBuffer(buffer, bytesReceived);
+            if (packet == null) return;
+
+            if (!SessionValid && packet.ServiceID != 0)
+            {
+                _preInitMessages.Add(packet);
+                return;
+            }
+
+            HandlePacket(packet);
+        }
+
         private void HandlePacket(INetworkMessage packet)
         {
             // Log the incoming packet.
@@ -310,52 +377,6 @@ namespace Imlight.Net
             }
 
             return record;
-        }
-        
-        private void SendToSocket(INetworkMessage message)
-        {
-            if (!Socket.Connected)
-            {
-                Log.Logger.Error($"SessionActor [{SessionID}] cannot send message [{message.GetType()}] " +
-                                 $"send failure: " +
-                                 $"Socket is not connected!");
-                return;
-            }
-            if (_isSending)
-            {
-                Log.Logger.Error($"SessionActor [{SessionID}] send failure: " +
-                                 $"Asynchronous send operation already in progress.");
-                return;
-            }
-
-            var data = MessageSerializer.SerializeMessageBinary(message);
-            _isSending = true;
-            _sendEventArgs.SetBuffer(data, 0, data.Length);
-            _sendEventArgs.UserToken = Socket;
-            _sendEventArgs.Completed += SendEventArgs_Completed;
-
-            var willRaiseEvent = Socket.SendAsync(_sendEventArgs);
-            if (!willRaiseEvent)
-            {
-                SendEventArgs_Completed(this, _sendEventArgs);
-            }
-
-            var scopedMessageName = message
-                .GetType()
-                .ToString()
-                .Split('.')[^1]
-                .Replace('+', '.');
-            Log.Logger.Debug($"SessionActor [{SessionID}] sent message [{scopedMessageName}]");
-        }
-
-        private void SendEventArgs_Completed(object sender, SocketAsyncEventArgs e)
-        {
-            _isSending = false;
-            if (e.SocketError != SocketError.Success)
-            {
-                Log.Logger.Error($"SessionActor [{SessionID}] send failure: {e.SocketError}");
-                return;
-            }
         }
         
         private void InitializeActiveSession(SERVICE_101_PROTOCOL.MSG_GETALLSERVICES message)
