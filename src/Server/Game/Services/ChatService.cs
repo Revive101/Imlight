816797b91f59c54a5e6bcb196a3a4a6620a48f4a
Phill <philltv.gaming@gmail.com>
2023-06-10 16:12:40 +0200

    feat(chatservice): add spell to book debug function

--- src/Server/Game/Services/ChatService.cs
+++ src/Server/Game/Services/ChatService.cs
@@ -2,6 +2,14 @@ using Akka.Actor;
 using WizUnraveler.Cache;
 using Imlight.Server.Shared.Networking;
 using Imlight.Server.Shared.Packets;
+using static WizUnraveler.Cache.TypeCache;
+using Imlight.Common.Utilities;
+using System.Text.Json;
+using Imlight.Server.Database;
+using System.Collections.Generic;
+using WizUnraveler.ObjectProperty;
+using WizUnraveler;
+using static WizUnraveler.ObjectProperty.ObjectSerializer;
 
 namespace Imlight.Server.Game.Services
 {
@@ -50,12 +58,16 @@ namespace Imlight.Server.Game.Services
                 Message = msg,
                 Selfless = true
             });
+
+            SendToSocket(new WIZARD_12_PROTOCOL.MSG_ADDSPELLTOBOOK()
+            {
+                SpellID = 2066
+            });
         }
-        
+
         [MessageHandler(typeof(GAME_5_PROTOCOL.MSG_REQASKSERVER))]
         private void ReceiveRequest(GAME_5_PROTOCOL.MSG_REQASKSERVER message)
         {
-            
         }
 
         [MessageHandler(typeof(GAME_5_PROTOCOL.MSG_CORE_EMOTE))]
