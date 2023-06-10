2d4f4e8a0c23d06ed7862b749d429d9bdef73b91
Phill <philltv.gaming@gmail.com>
2023-06-10 14:07:02 +0200

    feat: add InventoryService

---
+++ src/Server/Game/Services/InventoryService.cs
@@ -0,0 +1,23 @@
+﻿using Akka.Actor;
+using Imlight.Server.Shared.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imlight.Server.Game.Services
+{
+    public class InventoryService : MessageService
+    {
+        public InventoryService(SessionActor sessionActor) : base(sessionActor) { }
+
+        protected static Props Props(SessionActor parentActor)
+        {
+            return Akka.Actor.Props.Create(() => new InventoryService(parentActor));
+        }
+
+
+
+    }
+}
