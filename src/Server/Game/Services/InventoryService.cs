fad0078a45b36f92030bd1e054deeb84d97200c6
Phill <philltv.gaming@gmail.com>
2023-06-10 16:06:33 +0200

    feat: add InventoryService

--- src/Server/Game/Services/InventoryService.cs
+++ src/Server/Game/Services/InventoryService.cs
@@ -1,10 +1,13 @@
 ﻿using Akka.Actor;
+using Imlight.Common.Utilities;
 using Imlight.Server.Shared.Networking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using WizUnraveler.Cache;
 
 namespace Imlight.Server.Game.Services
 {
@@ -16,6 +19,19 @@ namespace Imlight.Server.Game.Services
         {
             return Akka.Actor.Props.Create(() => new InventoryService(parentActor));
         }
+
+        [MessageHandler(typeof(GAME_5_PROTOCOL.MSG_EQUIPITEM))]
+        private void ReceiveEquipItem(GAME_5_PROTOCOL.MSG_EQUIPITEM message)
+        {
+            SendToSocket(new GAME_5_PROTOCOL.MSG_EQUIPITEM()
+            {
+                ItemID = message.ItemID,
+                SlotName = message.SlotName,
+                IsEquip = message.IsEquip
+            });
+        }
+
+
 
 
 
