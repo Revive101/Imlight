f8b2aea3a1e456ae8ea573edaaefba3a1ea376df
Phill <philltv.gaming@gmail.com>
2023-06-11 01:11:44 +0200

    feat: add response for MSG_TRASHINVENTORYITEM

--- src/Server/Game/Services/InventoryService.cs
+++ src/Server/Game/Services/InventoryService.cs
@@ -1,5 +1,7 @@
 ﻿using Akka.Actor;
+using Imlight.Common.Utilities;
 using Imlight.Server.Shared.Networking;
+using Imlight.Server.Shared.Packets;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +9,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using WizUnraveler.Cache;
+using static WizUnraveler.Cache.GAME_5_PROTOCOL;
 
 namespace Imlight.Server.Game.Services
 {
@@ -22,6 +25,8 @@ namespace Imlight.Server.Game.Services
         [MessageHandler(typeof(GAME_5_PROTOCOL.MSG_EQUIPITEM))]
         private void ReceiveEquipItem(GAME_5_PROTOCOL.MSG_EQUIPITEM message)
         {
+            var coreObject = GetActiveCoreObject();
+
             SendToSocket(new GAME_5_PROTOCOL.MSG_EQUIPITEM()
             {
                 ItemID = message.ItemID,
@@ -30,9 +35,22 @@ namespace Imlight.Server.Game.Services
             });
         }
 
+        [MessageHandler(typeof(GAME_5_PROTOCOL.MSG_TRASHINVENTORYITEM))]
+        private void ReceiveTrashInventoryItem(GAME_5_PROTOCOL.MSG_TRASHINVENTORYITEM message)
+        {
+            SendToSocket(new GAME_5_PROTOCOL.MSG_TRASHINVENTORYITEM()
+            {
+                GlobalID = message.GlobalID,
+                TemplateID = message.TemplateID,
+            });
+        }
 
+        private TypeCache.CoreObject GetActiveCoreObject()
+        {
+            var msg = new CHARACTER_103_PROTOCOL.MSG_QUERYACTIVECHARACTER();
+            var response = AskSessionServices<CHARACTER_103_PROTOCOL.MSG_CHARACTER>(msg);
 
-
-
+            return response.CharacterObject;
+        }
     }
 }
