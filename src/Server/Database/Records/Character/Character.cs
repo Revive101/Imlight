dd775a074a4753e86bb5c8bf180a4959e8c4b119
Phill <philltv.gaming@gmail.com>
2023-06-23 23:52:13 +0200

    chore: add crowns mount

--- src/Server/Database/Records/Character/Character.cs
+++ src/Server/Database/Records/Character/Character.cs
@@ -139,7 +139,7 @@ namespace Imlight.Server.Database
 
                 new List<ulong>() { 4740, 4705, 5030, 39068, 1363076, 1475149,
                     1472644, 1317133, 1317126, 1317234, 1359455,
-                    1392077, 1352341, 87158, 87159, 87160 }.ForEach(templateId =>
+                    1392077, 1352341, 87158, 87159, 87160, 1540397 }.ForEach(templateId =>
                 {
                     CoreTemplate template = CoreObjectFactory.GetCoreTemplate(templateId);
                     Log.Logger.Debug("Template is: "+template.GetType());
