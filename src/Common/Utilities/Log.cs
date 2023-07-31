a891086f35fa32ab3aaf7a5b734737af9999eb22
Jay <compundbread@gmail.com>
2023-07-31 04:29:14 -0400

    feat: add calling method to log

--- src/Common/Utilities/Log.cs
+++ src/Common/Utilities/Log.cs
@@ -3,8 +3,11 @@
  * Proprietary and confidential.
  */
 
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using Serilog;
 using Serilog.Core;
@@ -26,8 +29,9 @@ namespace Imlight.Common.Utilities
             .MinimumLevel.Verbose()
             .Enrich.FromLogContext()
             .Enrich.With(new ThreadNameEnricher())
+            .Enrich.With(new CallingMethodEnricher())
             .WriteTo.Console(outputTemplate:
-                "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Thread}] [{Level:u5}] {Message:lj} {NewLine}{Exception}")
+                "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] [{Thread}] {CallingMethod} : {Message:lj} {NewLine}{Exception}")
             .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
             .CreateLogger();
     }
@@ -35,7 +39,7 @@ namespace Imlight.Common.Utilities
     internal class ThreadNameEnricher : ILogEventEnricher
     {
         public const string ThreadNamePropertyName = "Thread";
-        private const int MaxThreadNameLength = 15;
+        private const int MaxNameLength = 15;
 
         public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
         {
@@ -51,12 +55,78 @@ namespace Imlight.Common.Utilities
         {
             if (!string.IsNullOrEmpty(name))
             {
-                return name.Length > MaxThreadNameLength 
-                    ? name[..MaxThreadNameLength] 
-                    : name.PadLeft(MaxThreadNameLength);
+                return name.Length > MaxNameLength 
+                    ? name[..MaxNameLength] 
+                    : name.PadLeft(MaxNameLength);
             }
 
-            return "Main".PadLeft(MaxThreadNameLength);
+            return "Main".PadLeft(MaxNameLength);
+        }
+    }
+    
+    internal class CallingMethodEnricher : ILogEventEnricher
+    {
+        public const string CallingMethodPropertyName = "CallingMethod";
+        private const int MaxNameLength = 40;
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var frame = new StackFrame(6, false);
+            var method = frame.GetMethod();
+            if (method == null)
+                return;
+
+            var callingMethod = $"{method.ReflectedType?.Name}.{method.Name}";
+            if (IsStateMachineMethod(callingMethod))
+            {
+                callingMethod = GetCallerMethodNameFromAsyncStateMachine(frame);
+            }
+
+            var propertyMethod = propertyFactory.CreateProperty(CallingMethodPropertyName, GetConsistentSpacedName(callingMethod));
+            logEvent.AddPropertyIfAbsent(propertyMethod);
+        }
+
+        private static bool IsStateMachineMethod(string methodName)
+        {
+            return methodName.Contains('<');
+        }
+
+        private static string GetCallerMethodNameFromAsyncStateMachine(StackFrame frame)
+        {
+            var method = frame.GetMethod();
+            if (method == null) 
+                return "Unknown";
+            
+            var declaringType = method.DeclaringType;
+            if (declaringType == null) 
+                return "Unknown";
+            
+            // Get the string in between the < and >.
+            var stateMachineName = declaringType.Name;
+            var startIndex = stateMachineName.IndexOf('<') + 1;
+            var length = stateMachineName.LastIndexOf('>') - startIndex;
+            var stateMachineMethodName = stateMachineName.Substring(startIndex, length);
+            
+            // Get the declaring type name between the '.' and '+'. Then, scope it to the last '.'.
+            var declaringTypeName = declaringType.FullName;
+            startIndex = declaringTypeName.IndexOf('.') + 2;
+            length = declaringTypeName.LastIndexOf('+') - startIndex;
+            var declaringTypeMethodName = declaringTypeName.Substring(startIndex, length)
+                .Split('.')[^1];
+
+            return $"{declaringTypeMethodName}.{stateMachineMethodName}";
+        }
+        
+        private static string GetConsistentSpacedName(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                return name.Length > MaxNameLength 
+                    ? name[..MaxNameLength] 
+                    : name.PadRight(MaxNameLength);
+            }
+
+            return "Main".PadRight(MaxNameLength);
         }
     }
 }
