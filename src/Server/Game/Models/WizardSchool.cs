c8cecfa82efe406508428a02d836b508478bcce6
Jay <compundbread@gmail.com>
2023-08-06 16:14:31 -0400

    fix: WizardSchool string ID

--- src/Server/Game/Models/WizardSchool.cs
+++ src/Server/Game/Models/WizardSchool.cs
@@ -2,11 +2,11 @@ namespace Imlight.Server.Game.Models;
 
 public enum WizardSchool
 {
-    Fire = 0,
-    Ice = 1,
-    Storm = 2,
-    Life = 3,
-    Myth = 4,
-    Death = 5,
-    Balance = 6,
+    Fire = 2343174,
+    Ice = 72777,
+    Storm = 83375795,
+    Life = 2330892,
+    Myth = 2448141,
+    Death = 78318724,
+    Balance = 1027491821,
 }
