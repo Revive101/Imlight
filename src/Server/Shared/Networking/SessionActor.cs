df335cfa6b917ff4ecbb35a8a4557afa30ab58f0
Jay <compundbread@gmail.com>
2023-08-06 16:14:53 -0400

    fix: serviceId name change

--- src/Server/Shared/Networking/SessionActor.cs
+++ src/Server/Shared/Networking/SessionActor.cs
@@ -333,7 +333,7 @@ namespace Imlight.Server.Shared.Networking
                 return;
 
             var packet = GetPacketFromBuffer(e.Buffer, e.BytesTransferred);
-            if (packet != null && (SessionValid || packet.ServiceID == 0))
+            if (packet != null && (SessionValid || packet.ServiceId == 0))
             {
                 HandlePacket(packet);
             }
