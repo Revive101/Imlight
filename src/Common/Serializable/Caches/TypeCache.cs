25b2d41f55768d292f884bf6497da6953393206e
Rocket <me@rocketprogrammer.me>
2023-09-16 16:47:28 -0500

    serializable-cache: Push regenerated caches

--- src/Common/Serializable/Caches/TypeCache.cs
+++ src/Common/Serializable/Caches/TypeCache.cs
@@ -25437,6 +25437,11 @@ namespace Imlight.Common.Serializable.Caches
 			Guest_SKB_AZ_Ixcax_A_100 = 556560994,
 			Guest_SKB_AZ_Ixcax_A_150 = 556610146,
 			Guest_SKB_AZ_Ixcax_A_50 = 564951650,
+			MNT_GhoulsShovel = 2035610946,
+			PP_CrystalBdayDragon = 1897078804,
+			PP_SunGobblerBall = 1522469440,
+			PP_Sun_GreenGobbler = 1337690394,
+			MNT_2P_AutumnTreant = 1063435527,
 		}
 		
 		public enum eMountType
