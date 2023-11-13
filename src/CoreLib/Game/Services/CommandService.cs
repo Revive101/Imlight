00d002047b97a91808b278f01cafb2588a981537
gmrush <gmrush04@gmail.com>
2023-11-12 22:37:39 -0500

    chore: command service boilerplate

---
+++ src/CoreLib/Game/Services/CommandService.cs
@@ -0,0 +1,25 @@
+/* Copyright (C) Revive101 Development Team - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential.
+ */
+
+using System;
+using System.Linq;
+using Akka.Actor;
+using Akka.Util.Internal;
+using Imlight.Common.Caches;
+using Imlight.CoreLib.Shared.Networking;
+
+namespace Imlight.CoreLib.Game.Services;
+
+internal class CommandService : MessageService {
+
+    public CommandService(SessionActor sessionActor) : base(sessionActor) { }
+    protected static Props Props(SessionActor parentActor) {
+        return Akka.Actor.Props.Create(() => new CommandService(parentActor));
+    }
+
+    [MessageHandler(typeof(GAME_5_PROTOCOL.MSG_COMMAND))]
+    private void ReceiveCommand(GAME_5_PROTOCOL.MSG_COMMAND message) { }
+
+}
