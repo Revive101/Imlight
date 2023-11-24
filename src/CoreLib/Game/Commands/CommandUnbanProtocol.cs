1f57619f5723879590bb8a17b48546bdd5459c97
Jay <compundbread@gmail.com>
2023-11-24 13:37:33 -0500

    feat: add unban command protocol

---
+++ src/CoreLib/Game/Commands/CommandUnbanProtocol.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Linq;
+using Imlight.CoreLib.Login.Models;
+using Imlight.CoreLib.WizardData.Implementations;
+using Imlight.CoreLib.WizardData.Models;
+
+namespace Imlight.CoreLib.Game.Commands;
+
+internal class CommandUnbanProtocol : CommandProtocol {
+    internal override string Group { get; set; } = "unban";
+
+    [Command("account")]
+    [AuthRequired(AuthLevel.HallMonitor)]
+    [Alias("user", "username")]
+    private void UnbanCommand(string username) {
+        var account = AccountCollection.GetAccount(username);
+        if (account is null) {
+            InformSenderClient("Account not found.");
+            return;
+        }
+
+        if (!account.InfractionHistory.IsCurrentlyBanned) {
+            InformSenderClient("Account is not banned.");
+            return;
+        }
+
+        var result = account.WaiveCurrentBan(Context.Account.Username);
+        if (!result) {
+            InformSenderClient("Failed to waive ban.");
+            return;
+        }
+
+        InformSenderClient("Account unbanned.");
+    }
+
+    [Command("machine")]
+    [AuthRequired(AuthLevel.HallMonitor)]
+    [Alias("pc", "computer")]
+    private void UnbanMachineCommand(string machineId) {
+        if (!ulong.TryParse(machineId, out var machineIdLong)) {
+            InformSenderClient("Invalid machine ID");
+            return;
+        }
+
+        if (!InfractionCollection.IsMachineBanned(machineIdLong)) {
+            InformSenderClient("Machine is not banned");
+            return;
+        }
+
+        var result = InfractionCollection.RemoveMachineBan(machineIdLong);
+        if (!result) {
+            InformSenderClient("Failed to unban machine.");
+            return;
+        }
+
+        InformSenderClient("Machine unbanned.");
+    }
+
+    [Command("ip")]
+    [AuthRequired(AuthLevel.HallMonitor)]
+    private void UnbanIpCommand(string ip) {
+        if (!InfractionCollection.IsIpBanned(ip)) {
+            InformSenderClient("IP is not banned");
+            return;
+        }
+
+        var result = InfractionCollection.RemoveIpBan(ip);
+        if (!result) {
+            InformSenderClient("Failed to unban IP.");
+            return;
+        }
+
+        InformSenderClient("IP unbanned.");
+    }
+}
