4e74e14a2dd8e72307cb383ab8cf6181cf8af850
Jay <compundbread@gmail.com>
2023-11-30 15:33:39 -0500

    feat: add `gps` command

---
+++ src/CoreLib/Game/Commands/CommandDebugProtocol.cs
@@ -0,0 +1,26 @@
+using Imlight.CoreLib.AntiAmbrose;
+using Imlight.CoreLib.Login.Models;
+using Imlight.CoreLib.WizardData.Implementations;
+using Imlight.CoreLib.WizardData.Models;
+using System;
+using System.Text;
+
+namespace Imlight.CoreLib.Game.Commands;
+
+internal class CommandDebugProtocol : CommandProtocol {
+    internal override string Group { get; set; } = "debug";
+
+    [Command("gps")]
+    [AuthRequired(AuthLevel.HallMonitor)]
+    private void GpsCommand() {
+        var player = Context.CharacterObject;
+        var playerPosition = player.m_location;
+        var playerRotation = player.m_orientation;
+
+        var message = new StringBuilder();
+        message.AppendLine($"Position: {playerPosition}");
+        message.AppendLine($"Rotation: {playerRotation}");
+
+        InformSenderClient(message.ToString(), true);
+    }
+}
