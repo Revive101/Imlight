e56b69ecfd6c97602166f12744d1dcde93e1e8c1
Jay <compundbread@gmail.com>
2023-12-03 00:48:40 -0500

    fix: :heartbeat:

--- src/CoreLib/Game/Commands/CommandProtocol.cs
+++ src/CoreLib/Game/Commands/CommandProtocol.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using Imlight.CoreLib.WizardData.Models.Player;
 
 namespace Imlight.CoreLib.Game.Commands;
 
