335e2cdeff5af033c5b3738e8fa401937dc480c2
Jay <compundbread@gmail.com>
2023-12-12 17:48:22 -0500

    feat: add `CanonicalStatEffects`

---
+++ src/CoreLib/Game/Effects/CanonicalStatEffects.cs
@@ -0,0 +1,114 @@
+using Imlight.Common;
+using Imlight.Common.ObjectProperty.PropertyReflection;
+using Imlight.CoreLib.Shared.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Imlight.Common.Caches.TypeCache;
+
+namespace Imlight.CoreLib.Game.Effects;
+
+public static class CanonicalStatEffects {
+    private const string EffectTablePath = "GameEffectData/CanonicalStatEffects.xml";
+    private const string GameRuleDirectoryPrefix = "GameEffectRuleData/";
+    private static readonly string[] TimesHundredEffectNames = {
+        "Accuracy",
+        "Damage",
+        "Piercing",
+        "PowerPips",
+        "ReduceDamage",
+        "StunResistance"
+    };
+
+    private static readonly GameEffectTemplateList s_effectTable;
+    private static readonly WizardStatTable[] s_wizardStatTables;
+
+    // Constructor: Initialize the template table from the Root.wad.
+    static CanonicalStatEffects() {
+        s_effectTable = ResourceManager.LoadDeserializedFile<GameEffectTemplateList>(ResourceManager.RootWadName, EffectTablePath);
+        if (s_effectTable is null) {
+            Logger.Error("Could not find effect table {0} in {1}", Logger.Args(EffectTablePath, ResourceManager.RootWadName));
+            return;
+        }
+
+        // Iterate through each table and find the stat table name.
+        // This will be the literal name of the table in the Root.wad file.
+        var statTables = new List<WizardStatTable>();
+        foreach (var template in s_effectTable.m_effectTemplates) {
+            var statEffectTemplate = (WizStatisticEffectTemplate) template;
+            var statTableName = statEffectTemplate.m_statTableName;
+
+            // Search for this table in the Root.wad file.
+            var statTableDirectory = GameRuleDirectoryPrefix + statTableName + ".xml";
+            var statTable = ResourceManager.LoadDeserializedFile<WizardStatTable>(ResourceManager.RootWadName, statTableDirectory);
+            if (statTable is null) {
+                // There seem to be server side tables here. Not all of them are in the client.
+                // Logger.Error("Could not find stat table {0} in {1}", Logger.Args(statTableName, ResourceManager.RootWadName));
+            }
+
+            statTables.Add(statTable);
+        }
+
+        s_wizardStatTables = statTables.ToArray();
+        Logger.Information("Loaded {0} canonical stat tables", Logger.Args(s_wizardStatTables.Length));
+    }
+
+    /// <summary>
+    /// Retrieves the canonical stat value based on the provided <see cref="StatisticEffectInfo"/>.
+    /// </summary>
+    /// <param name="info">The <see cref="StatisticEffectInfo"/> containing the effect information.</param>
+    /// <returns>The calculated canonical stat value.</returns>
+    internal static float GetCanonicalStatValue(StatisticEffectInfo info) {
+        if (s_effectTable is null || s_wizardStatTables is null) {
+            Logger.Error("Effect table or stat tables are null. Cannot gather stat value.");
+            return 0;
+        }
+
+        var effectTemplate = GetEffectTemplate(info.m_effectName);
+        if (effectTemplate is null) {
+            Logger.Error("Could not find effect template {0}", Logger.Args(info.m_effectName));
+            return 0;
+        }
+
+        var statTable = GetStatTable(effectTemplate);
+        if (statTable is null) {
+            Logger.Error("Could not find stat table for template {0}", Logger.Args(effectTemplate.m_effectName));
+            return 0;
+        }
+
+        var stat = GetStatValue(statTable, info.m_lookupIndex);
+        var category = effectTemplate.m_effectCategory;
+        var isFlat = IsFlatEffect(info.m_effectName);
+
+        return CalculateStatValue(stat, category, isFlat);
+    }
+
+    private static GameEffectTemplate GetEffectTemplate(string effectName)
+        => s_effectTable.m_effectTemplates.FirstOrDefault(x => x.m_effectName == effectName);
+
+    private static WizardStatTable GetStatTable(GameEffectTemplate effectTemplate) {
+        var tableName = ((WizStatisticEffectTemplate)effectTemplate).m_statTableName;
+        return s_wizardStatTables.FirstOrDefault(x => x is not null && x.m_tableName == tableName);
+    }
+
+    private static float GetStatValue(WizardStatTable statTable, int lookupIndex) {
+        if (lookupIndex >= statTable.m_statVector.Count) {
+            Logger.Error("Invalid lookup index {0} for stat table {1}", Logger.Args(lookupIndex, statTable.m_tableName));
+            return 0;
+        }
+
+        return statTable.m_statVector[lookupIndex];
+    }
+
+    private static bool IsFlatEffect(string effectName)
+        => effectName.ToString().Contains("Flat");
+
+    private static float CalculateStatValue(float stat, string category, bool isFlat) {
+        if (TimesHundredEffectNames.Contains(category) && !isFlat) {
+            return stat * 100f;
+        }
+        else {
+            return stat;
+        }
+    }
+}
--- src/CoreLib/Game/Effects/GameEffectFactory.cs
+++ src/CoreLib/Game/Effects/GameEffectFactory.cs
@@ -56,38 +56,39 @@ internal static class GameEffectFactory {
             m_effectNameID = StringHash.Compute(info.m_effectName),
             m_itemSlotID = itemSlotId
         };
+        var val = CanonicalStatEffects.GetCanonicalStatValue(info);
 
         // Read it and weep.
         var effectName = info.m_effectName.ToString();
         switch (effectName) {
-            case var _ when effectName.Contains("MaxMana")         : effect.m_manaBonus = info.m_lookupIndex + 98;                          break;
-            case var _ when effectName.Contains("MaxHealth")       : effect.m_hitPointBonus = info.m_lookupIndex + 1;                       break;
-            case var _ when effectName.Contains("MaxEnergy")       : effect.m_energyBonus = info.m_lookupIndex + 1;                         break;
-            case var _ when effectName.Contains("FlatReduceDamage"): effect.m_damageReduceFlat = info.m_lookupIndex + 1;                    break;
-            case var _ when effectName.Contains("FlatDamage")      : effect.m_damageBonusFlat = info.m_lookupIndex + 1;                     break;
-            case var _ when effectName.Contains("CriticalHit")     : effect.m_criticalHitRating = info.m_lookupIndex + 1;                   break;
-            case var _ when effectName.Contains("Block")           : effect.m_blockRating = info.m_lookupIndex + 1;                         break;
-            case var _ when effectName.Contains("PipConversion")   : effect.m_pipConversionRating = info.m_lookupIndex + 1;                 break;
-            case var _ when effectName.Contains("ShadowPipRating") : effect.m_shadowPipRating = info.m_lookupIndex + 1;                     break;
-            case var _ when effectName.Contains("PowerPip")        : effect.m_powerPipBonusPercent = (info.m_lookupIndex - 99) / 100f;      break;
-            case var _ when effectName.Contains("ReduceDamage")    : effect.m_damageReducePercent = (info.m_lookupIndex - 99) / 100f;       break;
-            case var _ when effectName.Contains("Damage")          : effect.m_damageBonusPercent = (info.m_lookupIndex - 99) / 100f;        break;
-            case var _ when effectName.Contains("Accuracy")        : effect.m_accuracyBonusPercent = (info.m_lookupIndex - 99) / 100f;      break;
-            case var _ when effectName.Contains("ArmorPiercing")   : effect.m_armorPiercingBonusPercent = (info.m_lookupIndex - 99) / 100f; break;
-            case var _ when effectName.Contains("FishingLuck")     : effect.m_fishingLuckBonusPercent = (info.m_lookupIndex - 99) / 100f;   break;
-            case var _ when effectName.Contains("IncHealing")      : effect.m_healIncBonusPercent = (info.m_lookupIndex - 99) / 100f;       break;
-            case var _ when effectName.Contains("LifeHealing")     : effect.m_healBonusPercent = (info.m_lookupIndex - 99) / 100f;          break;
-            case var _ when effectName.Contains("StunResistance")  : effect.m_stunResistancePercent = (info.m_lookupIndex - 99) / 100f;     break;
-            case var _ when effectName.Contains("XPPercent")       : effect.m_expPercent = (info.m_lookupIndex - 99) / 100f;                break;
-            case var _ when effectName.Contains("GoldPercent")     : effect.m_goldPercent = (info.m_lookupIndex - 99) / 100f;               break;
-            case "CanonicalStormMastery"                           : effect.m_stormMastery = 1;                                             break;
-            case "CanonicalFireMastery"                            : effect.m_fireMastery = 1;                                              break;
-            case "CanonicalIceMastery"                             : effect.m_iceMastery = 1;                                               break;
-            case "CanonicalLifeMastery"                            : effect.m_lifeMastery = 1;                                              break;
-            case "CanonicalDeathMastery"                           : effect.m_deathMastery = 1;                                             break;
-            case "CanonicalMythMastery"                            : effect.m_mythMastery = 1;                                              break;
-            case "CanonicalBalanceMastery"                         : effect.m_balanceMastery = 1;                                           break;
-            default                                                :                                                                        break;
+            case var _ when effectName.Contains("MaxMana") : effect.m_manaBonus = val; break;
+            case var _ when effectName.Contains("MaxHealth") : effect.m_hitPointBonus = val; break;
+            case var _ when effectName.Contains("MaxEnergy") : effect.m_energyBonus = val; break;
+            case var _ when effectName.Contains("FlatReduceDamage"): effect.m_damageReduceFlat = val; break;
+            case var _ when effectName.Contains("FlatDamage") : effect.m_damageBonusFlat = val; break;
+            case var _ when effectName.Contains("CriticalHit") : effect.m_criticalHitRating = val; break;
+            case var _ when effectName.Contains("Block") : effect.m_blockRating = val; break;
+            case var _ when effectName.Contains("PipConversion") : effect.m_pipConversionRating = val; break;
+            case var _ when effectName.Contains("ShadowPipRating") : effect.m_shadowPipRating = val; break;
+            case var _ when effectName.Contains("PowerPip") : effect.m_powerPipBonusPercent = val; break;
+            case var _ when effectName.Contains("ReduceDamage") : effect.m_damageReducePercent = val; break;
+            case var _ when effectName.Contains("Damage") : effect.m_damageBonusPercent = val; break;
+            case var _ when effectName.Contains("Accuracy") : effect.m_accuracyBonusPercent = val; break;
+            case var _ when effectName.Contains("ArmorPiercing") : effect.m_armorPiercingBonusPercent = val; break;
+            case var _ when effectName.Contains("FishingLuck") : effect.m_fishingLuckBonusPercent =val; break;
+            case var _ when effectName.Contains("IncHealing") : effect.m_healIncBonusPercent = val; break;
+            case var _ when effectName.Contains("LifeHealing") : effect.m_healBonusPercent = val; break;
+            case var _ when effectName.Contains("StunResistance") : effect.m_stunResistancePercent = val; break;
+            case var _ when effectName.Contains("XPPercent") : effect.m_expPercent = val; break;
+            case var _ when effectName.Contains("GoldPercent") : effect.m_goldPercent = val; break;
+            case "CanonicalStormMastery" : effect.m_stormMastery = 1; break;
+            case "CanonicalFireMastery" : effect.m_fireMastery = 1; break;
+            case "CanonicalIceMastery" : effect.m_iceMastery = 1; break;
+            case "CanonicalLifeMastery" : effect.m_lifeMastery = 1; break;
+            case "CanonicalDeathMastery" : effect.m_deathMastery = 1; break;
+            case "CanonicalMythMastery" : effect.m_mythMastery = 1; break;
+            case "CanonicalBalanceMastery" : effect.m_balanceMastery = 1; break;
+            default : break;
         }
 
         return effect;
