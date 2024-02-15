28da3c4b814f251885fcfb6d9c76995ef915578b
Jay <compundbread@gmail.com>
2024-02-15 13:47:12 -0500

    feat: `EquippedItemInfo` abstraction

--- src/CoreLib/WizardData/Implementations/ItemHelper.cs
+++ src/CoreLib/WizardData/Implementations/ItemHelper.cs
@@ -2,6 +2,7 @@ using Imlight.Common;
 using Imlight.Common.Cryptography;
 using Imlight.Common.ObjectProperty.PropertyReflection;
 using Imlight.CoreLib.Shared.Resources;
+using Imlight.CoreLib.WizardData.Models.Player;
 using System;
 using System.Linq;
 using static Imlight.Common.Caches.TypeCache;
@@ -9,10 +10,6 @@ using static Imlight.Common.Caches.TypeCache;
 namespace Imlight.CoreLib.WizardData.Implementations;
 
 internal static class ItemHelper {
-    private static readonly string[] s_slotNames = {
-        "Hat", "Robe", "Shoes", "Weapon", "Athame", "Amulet", "Ring", "Mount", "Deck",
-    };
-
     /// <summary>
     /// Gets the <see cref="EquippedItemInfoList"/> for a <see cref="Wizard"/>. This is a lightweight version of the
     /// actual equipment that is used to publicly display the character's equipment.
@@ -41,28 +38,23 @@ internal static class ItemHelper {
     }
 
     /// <summary>
-    /// Gets the slot hash of a WizClientObjectItem.
+    /// Gets the slot of a WizClientObjectItem from the template adjectives.
     /// </summary>
-    /// <param name="item">The WizClientObjectItem to get the slot hash from.</param>
-    /// <returns>The slot hash of the item, or 0 if the item template is null or the adjective list count is less than 2.</returns>
-    internal static uint GetItemSlotHash(WizClientObjectItem item) {
-        var template = GetItemTemplate(item);
-        if (template == null) {
-            return 0;
-        }
+    /// <param name="itemTemplate"></param>
+    /// <returns>The EquipmentSlot of the item; null if a matching adjective is not found.</returns>
+    internal static EquipmentSlot GetItemSlot(WizItemTemplate itemTemplate) {
+        // Iterate through the EquipmentSlot enum and return the first slot that matches the item's slot.
+        foreach (var slot in Enum.GetValues(typeof(EquipmentSlot)).Cast<EquipmentSlot>()) {
+            // Sanitize the slot name.
+            var slotName = slot.ToString().Split('.')[^1];
 
-        // Iterate through the slot names and return the hash of the first slot name that matches the item's slot.
-        foreach (var slotName in s_slotNames) {
-            if (template.m_adjectiveList.Any(adj => string.Equals(adj, slotName, StringComparison.OrdinalIgnoreCase))) {
-                return StringHash.Compute(slotName);
+            // Check if any of the items adjectives match the slot name.
+            if (itemTemplate.m_adjectiveList.Any(adj => string.Equals(adj, slotName, StringComparison.OrdinalIgnoreCase))) {
+                return slot;
             }
         }
 
-        // Log that we couldn't find the slot name, and print all the adjectives.
-        Logger.Error($"Couldn't find slot name for item {0} with adjectives: {1}",
-            Logger.Args(item.m_templateID, string.Join(", ", template.m_adjectiveList)));
-
-        return 0;
+        return null;
     }
 
     /// <summary>
@@ -71,18 +63,8 @@ internal static class ItemHelper {
     /// <param name="template">The item template.</param>
     /// <returns>The hash value of the item slot.</returns>
     internal static uint GetItemSlotHash(WizItemTemplate template) {
-        if (template == null) {
-            return 0;
-        }
-
-        // Get the slot hash from the item template.
-        if (template.m_adjectiveList.Count < 2) {
-            return 0;
-        }
-        else {
-            // The second adjective is the slot name.
-            var slotName = template.m_adjectiveList[1];
-            return StringHash.Compute(slotName);
-        }
+        var slot = GetItemSlot(template);
+        var slotName = slot?.ToString().Split('.')[^1];
+        return slot is null ? 0 : StringHash.ComputeHash(slotName);
     }
 }
--- src/CoreLib/WizardData/Models/Player/EquipmentSlot.cs
+++ src/CoreLib/WizardData/Models/Player/EquipmentSlot.cs
@@ -3,9 +3,15 @@
  * Proprietary and confidential.
  */
 
+using Imlight.Common.Cryptography;
+using Imlight.Common.ObjectProperty.PropertyReflection;
+using Imlight.CoreLib.WizardData.Implementations;
+using System;
+using static Imlight.Common.Caches.TypeCache;
+
 namespace Imlight.CoreLib.WizardData.Models.Player;
 
-public enum EquipmentSlot {
+public enum EquipmentSlotType {
     Hat,
     Robe,
     Shoes,
@@ -16,4 +22,24 @@ public enum EquipmentSlot {
     Pet,
     Mount,
     Deck
+}
+
+/// <summary>
+/// Represents a slot in the player's equipment. This is an abstraction on top of Wizard101's
+/// type to make it easier to work with.
+/// </summary>
+[Serializable]
+public class EquipmentSlot : IClientTypeProvider<EquippedSlotInfo> {
+    public EquipmentSlotType SlotType { get; set; }
+    public GID ItemId { get; set; }
+    public DateTime EquippedSince { get; set; }
+
+    public EquippedSlotInfo GetClientTypeAlternative() {
+        // Remove the prefix from the slot type.
+        var slotType = SlotType.ToString().Split('.')[^1];
+        return new EquippedSlotInfo {
+            m_itemID = ItemId,
+            m_itemSlotNameID = StringHash.Compute(slotType),
+        };
+    }
 }
