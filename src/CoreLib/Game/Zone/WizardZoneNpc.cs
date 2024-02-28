213aa32f11c6dcce6ae178a012bb158a57b3e0ff
gmrush <gmrush04@gmail.com>
2024-02-28 18:35:12 -0500

    feat: npc interact beginnings

--- src/CoreLib/Game/Zone/WizardZoneNpc.cs
+++ src/CoreLib/Game/Zone/WizardZoneNpc.cs
@@ -9,6 +9,7 @@ using Imlight.Common.Cryptography;
 using Imlight.Common.ObjectProperty;
 using Imlight.CoreLib.Shared.Packets;
 using System.Linq;
+using System.Collections.Generic;
 using static Imlight.Common.Caches.TypeCache;
 
 namespace Imlight.CoreLib.Game.Zone;
@@ -58,4 +59,77 @@ public class WizardZoneNpc : WizardZoneObject {
         Sender.Tell(new ZONE_102_PROTOCOL.MSG_ADDOBJECTRSP());
     }
 
+    protected override void OnPlayerInteractionEnter(CoreObject player, IActorRef suspect) {
+        base.OnPlayerInteractionEnter(player, suspect);
+
+        if (Template is not GameObjectTemplate gameObjTemplate) {
+            return;
+        }
+
+        var serializer = new ObjectSerializer()
+          .OnBehaviors(SerializerOptions.Behaviors.None)
+          .OnPropertyMask((SerializerOptions.PropertyFlags) 4);
+
+        var npcName = gameObjTemplate.m_objectName.ToString().ToLower();
+        var debugName = ActiveGameObject.m_debugName.ToString().ToLower();
+        if (s_shopKeeperNameGiveaways.Any(npcName.Contains) || s_explorerNames.Any(n => debugName == n)) {
+            var madlibList = new List<MadlibArg> {
+                new MadlibArgT_std_string() {
+                    m_madlibArgument = "Persona,First_00000039",
+                    m_madlibToken = "FIRSTNAME"
+                },
+                new MadlibArgT_std_string() {
+                    m_madlibArgument = "Persona,Last_00000033",
+                    m_madlibToken = "LASTNAME"
+                },
+                new MadlibArgT_std_string() {
+                    m_madlibArgument = "Title_00000018",
+                    m_madlibToken = "TITLE"
+                },
+                new MadlibArgT_std_string() {
+                    m_madlibArgument = "",
+                    m_madlibToken = "NICKNAME"
+                },
+                new MadlibArgT_std_string() {
+                    m_madlibArgument = "NPCFormats_First_Last",
+                    m_madlibToken = "FULLNAME"
+                },
+            };
+
+            var serviceOptions = new List<ServiceOptionBase> {
+                new EquipmentShopOption() {
+                    m_displayKey = "GUI_ShopOptionEquipment",
+                    m_forceInteract = false,
+                    m_iconKey = "Shopping",
+                    m_serviceIndex = 0,
+                    m_serviceName = "WizShoppingService"
+                }
+            };
+
+            var serviceMementoBase = new ServiceMementoBase() {
+                m_bTurnPlayerToFace = true,
+                m_clickToInteractOnly = false,
+                m_npcFarewellSound = "",
+                m_npcGreetingSound = "",
+                m_npcIcon = "GUI/NpcPortraits/Art_Portrait_Unknown.dds",
+                m_npcNameKey = "NPCFormats_First_Last",
+                m_npcTextKey = "GUI_NPCInteractText",
+                m_personaMadlibs = new MadlibBlock() {
+                    m_blockToken = "NPC",
+                    m_madlibs = madlibList
+                },
+                m_serviceOptions = serviceOptions
+            };
+
+            var data = serializer.Serialize(serviceMementoBase);
+
+            var npcOptionsMsg = new QUEST_MESSAGES_52_PROTOCOL.MSG_SENDNPCOPTIONS {
+                MobileID = ActiveGameObject.m_globalID,
+                Options = data,
+                Reinteract = 0
+            };
+
+            suspect.Tell(npcOptionsMsg);
+        }
+    }
 }
