efd463ddbaa6a27e70441d2a6b500c011a34df87
Jay <compundbread@gmail.com>
2024-03-10 19:22:56 -0400

    ref: `MoveService` cleanup

--- src/CoreLib/Game/Services/MoveService.cs
+++ src/CoreLib/Game/Services/MoveService.cs
@@ -15,30 +15,31 @@ using SharpDX;
 namespace Imlight.CoreLib.Game.Services;
 
 internal class MoveService : MessageService {
+    private const byte MARK_MANA_COST_PERCENT = 20;
 
-    private const byte MarkManaCostPercent = 20;
+    private TypeCache.CoreObject _activeCoreObject;
 
     public MoveService(SessionActor sessionActor) : base(sessionActor) { }
 
-    protected static Props Props(SessionActor parentActor) {
-        return Akka.Actor.Props.Create(() => new MoveService(parentActor));
-    }
+    protected static Props Props(SessionActor parentActor)
+        => Akka.Actor.Props.Create(() => new MoveService(parentActor));
 
     [MessageHandler(typeof(GAME_5_PROTOCOL.MSG_CLIENTMOVE))]
     private void ReceiveClientMove(GAME_5_PROTOCOL.MSG_CLIENTMOVE message) {
         // MoveService saves the location of the player's game object.
         // CharacterService saves the location of the player's character persistently.
 
+        this._activeCoreObject ??= GetActiveGameObject();
+
         // Restore actual location information, as it is compressed by a factor of 4 and unsigned.
         // Yaw is represented in radians in the client, but transmitted to the server as degrees.
-        var x = unchecked((short) message.LocationX) * 4.0f;
-        var y = unchecked((short) message.LocationY) * 4.0f;
-        var z = unchecked((short) message.LocationZ) * 4.0f;
-        var direction = (float) (message.Direction * Math.PI * 2 / 250);
+        var deflatedPos = new Vector3(message.LocationX, message.LocationY, message.LocationZ);
+        var deflatedDir = message.Direction;
+        var inflatedPos = DecompressLocation(deflatedPos);
+        var inflatedDir = DecompressDirection(deflatedDir);
 
-        var activeCharacterObject = GetActiveGameObject();
-        activeCharacterObject.m_location = new Vector3(x, y, z);
-        activeCharacterObject.m_orientation = new Vector3(0, 0, direction);
+        _activeCoreObject.m_location = inflatedPos;
+        _activeCoreObject.m_orientation = new Vector3(0, 0, inflatedDir);
 
         // Broadcast the move to all other players in the zone.
         BroadcastClientMove(message);
@@ -46,15 +47,8 @@ internal class MoveService : MessageService {
     }
 
     [MessageHandler(typeof(GAME_5_PROTOCOL.MSG_CLIENTMOVESTATE))]
-    private void ReceiveClientMoveState(GAME_5_PROTOCOL.MSG_CLIENTMOVESTATE message) {
-        var globalId = GetActiveGameObject().m_globalID;
-
-        var stateMsg = new GAME_5_PROTOCOL.MSG_MOVESTATE {
-            NewState = message.NewState,
-            GlobalID = globalId
-        };
-        ZoneBroadcast(stateMsg);
-    }
+    private void ReceiveClientMoveState(GAME_5_PROTOCOL.MSG_CLIENTMOVESTATE message)
+        => BroadcastClientMoveState(message);
 
     [MessageHandler(typeof(GAME_5_PROTOCOL.MSG_JUMP))]
     private void ReceiveClientJump(GAME_5_PROTOCOL.MSG_JUMP message) {
@@ -67,7 +61,7 @@ internal class MoveService : MessageService {
         var wizard = GetActiveWizard();
 
         // If the character doesn't have enough mana, return.
-        if (wizard.GameStats.m_currentMana < wizard.GameStats.m_baseMana / MarkManaCostPercent) {
+        if (wizard.GameStats.m_currentMana < wizard.GameStats.m_baseMana / MARK_MANA_COST_PERCENT) {
             var failedRsp = new GAME_5_PROTOCOL.MSG_MARK_LOCATION_RESPONSE {
                 Result = 0,
                 MarkType = "1"
@@ -79,7 +73,7 @@ internal class MoveService : MessageService {
         wizard.SetMarkedLocation(wizard.Location, wizard.Orientation, wizard.Zone);
 
         var oldMana = wizard.GameStats.m_currentMana;
-        var newMana = oldMana - (wizard.GameStats.m_baseMana * ((float) MarkManaCostPercent / 100));
+        var newMana = oldMana - (wizard.GameStats.m_baseMana * ((float) MARK_MANA_COST_PERCENT / 100));
         wizard.GameStats.m_currentMana = (int) newMana;
 
         SendToSocket(new WIZARD_12_PROTOCOL.MSG_UPDATEMANA() {
@@ -110,12 +104,15 @@ internal class MoveService : MessageService {
 
         // If we are in the same zone as the marked location, teleport to it.
         if (wizard.MarkedZone == wizard.Zone) {
+            var deflatedPos = CompressLocation(wizard.MarkedLocation);
+            var deflatedDir = CompressDirection(wizard.MarkedOrientation.Z);
+
             var serverTeleportRsp = new GAME_5_PROTOCOL.MSG_SERVERTELEPORT {
                 // Compress the location by a factor of 4 and convert to unsigned.
-                Direction = (byte) (wizard.MarkedOrientation.Z / Math.PI / 2 * 250),
-                LocationX = (ushort) (wizard.MarkedLocation.X / 4),
-                LocationY = (ushort) (wizard.MarkedLocation.Y / 4),
-                LocationZ = (ushort) (wizard.MarkedLocation.Z / 4),
+                Direction = deflatedDir,
+                LocationX = (ushort) deflatedPos.X,
+                LocationY = (ushort) deflatedPos.Y,
+                LocationZ = (ushort) deflatedPos.Z,
                 MobileID = wizard.GameObject.m_nMobileID,
             };
             var recallRsp = new GAME_5_PROTOCOL.MSG_MARK_LOCATION_RESPONSE {
@@ -136,13 +133,7 @@ internal class MoveService : MessageService {
         }
         // If we're not in the same zone, send a zone transfer prior to the server teleport.
         else {
-            var zoneTransfer = new ZONE_102_PROTOCOL.MSG_ZONETRANSFER {
-                DestinationLocation =
-                    Util.GetCompactStringFromVector(wizard.MarkedLocation, wizard.MarkedOrientation),
-                DestinationZone = wizard.MarkedZone,
-                SendToClient = true,
-            };
-            TellOtherServices(zoneTransfer);
+            DoZoneTransfer(wizard.MarkedZone);
 
             var recallRsp = new GAME_5_PROTOCOL.MSG_MARK_LOCATION_RESPONSE {
                 Result = 1,
@@ -153,25 +144,54 @@ internal class MoveService : MessageService {
     }
 
     private void BroadcastClientMove(GAME_5_PROTOCOL.MSG_CLIENTMOVE message) {
-        // Query the mobile ID from the CharacterService
-        var mobileId = GetActiveGameObject().m_nMobileID;
+        this._activeCoreObject ??= GetActiveGameObject();
 
         var serverMoveMsg = new GAME_5_PROTOCOL.MSG_SERVERMOVE {
             LocationX = message.LocationX,
             LocationY = message.LocationY,
             LocationZ = message.LocationZ,
             Direction = message.Direction,
-            MobileID = mobileId,
+            MobileID = _activeCoreObject.m_nMobileID,
         };
         ZoneBroadcast(serverMoveMsg);
     }
 
+    private void BroadcastClientMoveState(GAME_5_PROTOCOL.MSG_CLIENTMOVESTATE message) {
+        this._activeCoreObject ??= GetActiveGameObject();
+
+        var stateMsg = new GAME_5_PROTOCOL.MSG_MOVESTATE {
+            NewState = message.NewState,
+            GlobalID = _activeCoreObject.m_globalID
+        };
+        ZoneBroadcast(stateMsg);
+    }
+
     private void SendZoneInteractionFishRequest() {
-        var characterObj = GetActiveGameObject();
         var msg = new ZONE_102_PROTOCOL.MSG_FISHINTERACTION() {
-            CoreObject = characterObj,
+            CoreObject = _activeCoreObject,
             Suspect = SessionActor.ActorRef
         };
+
         TellOtherServices(msg);
     }
+
+    private static Vector3 CompressLocation(Vector3 location) => new Vector3(
+            (float) Math.Round(location.X / 4),
+            (float) Math.Round(location.Y / 4),
+            (float) Math.Round(location.Z / 4)
+        );
+
+    private static byte CompressDirection(float direction)
+        => (byte) Math.Round(direction / Math.PI / 2 * 250);
+
+    private static Vector3 DecompressLocation(Vector3 location) {
+        var x = unchecked((short) location.X) * 4.0f;
+        var y = unchecked((short) location.Y) * 4.0f;
+        var z = unchecked((short) location.Z) * 4.0f;
+
+        return new Vector3(x, y, z);
+    }
+
+    private static float DecompressDirection(float direction)
+        => (float) (direction * Math.PI * 2 / 250);
 }
