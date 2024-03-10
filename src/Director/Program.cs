90ffad2ceaf2f72479d4f79686239b9dd9c71bfd
MoMisGrand <77238866+MoMisGrand@users.noreply.github.com>
2024-03-10 00:46:31 +0000

    -Acc

--- src/Director/Program.cs
+++ src/Director/Program.cs
@@ -130,6 +130,7 @@ internal static class Program {
         // Dev accounts; Hi, devs! Feel free to make your own account and add it here.
         new Jooty("jooty", "2342", "jay@r101net", AuthLevel.Administrator);
         new Joji("joji", "jootysocoollike", "joji@r101.net", AuthLevel.Administrator);
+        new MoMi("MoMi", "2109", "MoMi@r101.net", AuthLevel.Administrator);
         new Phill("phill", "gay", "phill@r101.net", AuthLevel.Administrator);
         new Jeff("jeff", "jefffakename", "jeff@r101.net", AuthLevel.Administrator);
         new Rocket("rocket", "7969", "rocket@r101.net", AuthLevel.Developer);
