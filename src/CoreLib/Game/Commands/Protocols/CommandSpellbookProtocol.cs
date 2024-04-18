1a87463a27d28e6552016352e3fd8a91a9ade371
MoMisGrand <momisgrand909@gmail.com>
2024-04-18 16:44:17 +0200

    fix:unlearnall

--- src/CoreLib/Game/Commands/Protocols/CommandSpellbookProtocol.cs
+++ src/CoreLib/Game/Commands/Protocols/CommandSpellbookProtocol.cs
@@ -10,10 +10,13 @@ using Imlight.Common.Configuration;
 using Imlight.Common.Cryptography;
 using Imlight.Common.ObjectProperty;
 using Imlight.CoreLib.Game.Spells;
+using Imlight.CoreLib.Shared.Behaviors;
 using Imlight.CoreLib.Shared.Packets;
 using Imlight.CoreLib.Shared.Resources;
+using Imlight.CoreLib.WizardData.Implementations;
 using Imlight.CoreLib.WizardData.Models.Player;
 using System;
+using System.Collections.Generic;
 using static Imlight.Common.Caches.TypeCache;
 using static Raven.Client.Documents.Commands.MultiGet.GetRequest;
 
@@ -103,18 +106,27 @@ internal class CommandSpellbookProtocol : CommandProtocol {
     [AuthRequired(AuthLevel.QualityAssurance)]
     private void UnlearnallSpellCommand() {
 
-        if (Context.Character.SpellbookBehavior.LearnedSpellTemplateIds != null){ 
-        foreach (var spell in Context.Character.SpellbookBehavior.LearnedSpellTemplateIds) {
+        if (Context.Character.SpellbookBehavior.LearnedSpellTemplateIds != null) {
+            List<uint> spellList = new();
+            foreach (var spell in Context.Character.SpellbookBehavior.LearnedSpellTemplateIds) {
+                spellList.Add(spell);
+            }
 
-            
-            var clientMsg = new WIZARD_12_PROTOCOL.MSG_REMOVESPELLFROMBOOK {
-                SpellID = (int) spell
-            };
-            Context.SessionActor.Tell(clientMsg);
-            Context.Character.UnlearnSpell(spell);
-            InformSenderClient($"You have unlearned the spell {spell}.");
+            foreach (var spell in spellList) {
+
+
+                var clientMsg = new WIZARD_12_PROTOCOL.MSG_REMOVESPELLFROMBOOK {
+                    SpellID = (int) spell
+                };
+                Context.SessionActor.Tell(clientMsg);
+
+                Context.Character.SpellbookBehavior.RemoveSpellFromBook(spell);
+
+                // Persistent save.
+                WizardCollection.UnlearnSpell(Context.Character, spell);
+                InformSenderClient($"You have unlearned the spell {spell}.");
+            }
         }
-    }
         else {
             InformSenderClient($"No Spells To Unlearn");
         }
