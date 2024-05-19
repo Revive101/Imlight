9fb4a743ec99657a632033d6fd925cdfb4b42bea
Jay <compundbread@gmail.com>
2024-05-19 18:34:46 -0400

    ref: move work into `CombatEffectProcessor`

---
+++ src/CoreLib/Game/Combat/CombatEffectProcessor.cs
@@ -0,0 +1,89 @@
+/* Copyright (C) Revive101 Development Team - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Imlight.Common.Caches.TypeCache;
+
+namespace Imlight.CoreLib.Game.Combat;
+
+/// <summary>
+/// Processes the effects of a spell, choosing which effects are relevant and applying them to the targets.
+/// This class is responsible for handling the logic of how effects are applied to targets.
+/// The actual application of effects happens in <see cref="CombatEffectApplicator"/>.
+/// </summary>
+internal static class CombatEffectProcessor {
+    internal static bool ProcessSpellEffects(QueuedCombatAction action, CombatEffectStack effectStack, ref CombatAction combatAction, ref float cinematicTime) {
+        var spellWorthCasting = false;
+
+        foreach (var spellEffect in action.SpellTemplate.m_effects) {
+            var chosenEffect = spellEffect;
+
+            if (spellEffect is RandomSpellEffect randomSpellEffect) {
+                chosenEffect = ChooseRandomEffect(randomSpellEffect, effectStack);
+            }
+
+            var targets = GetEffectTargets(chosenEffect, action.SpellCaster, action.SelectedTarget);
+            if (targets.Length == 0) {
+                continue;
+            }
+
+            if (!spellWorthCasting && targets.Any(x => x.IsAlive)) {
+                spellWorthCasting = true;
+            }
+
+            UpdateCombatActionTargets(ref combatAction, targets);
+
+            cinematicTime += CombatEffectApplicator.ApplyEffect(chosenEffect, action.SpellCaster, targets);
+        }
+
+        return spellWorthCasting;
+    }
+
+    private static SpellEffect ChooseRandomEffect(RandomSpellEffect randomSpellEffect, CombatEffectStack effectStack) {
+        var count = randomSpellEffect.m_effectList.Count;
+        var randomEffectIndex = new Random().Next(0, count);
+        var chosenEffect = randomSpellEffect.m_effectList[randomEffectIndex];
+
+        effectStack.PushRandomEffectChoice(randomEffectIndex);
+
+        return chosenEffect;
+    }
+
+    private static void UpdateCombatActionTargets(ref CombatAction combatAction, IEnumerable<CombatDuelActorSubCircle> targets) {
+        foreach (var target in targets) {
+            if (!combatAction.m_targetSubcircleList.Contains(target.SlotIndex)) {
+                combatAction.m_targetSubcircleList.Add(target.SlotIndex);
+            }
+        }
+    }
+
+    private static CombatDuelActorSubCircle[] GetEffectTargets(SpellEffect effect, CombatDuelActorSubCircle caster, CombatDuelActorSubCircle target) {
+        var targets = Array.Empty<CombatDuelActorSubCircle>();
+        var _activeSubCircles = caster.DuelActor.ActiveSubCircles;
+
+        switch (effect.m_effectTarget) {
+            case SpellEffect.kEffectTarget.kEnemySingle:
+            case SpellEffect.kEffectTarget.kFriendlySingle:
+                targets = new[] { target };
+                break;
+            case SpellEffect.kEffectTarget.kSelf:
+            case SpellEffect.kEffectTarget.kInvalidTarget:
+                targets = new[] { caster };
+                break;
+            case SpellEffect.kEffectTarget.kFriendlyTeam:
+            case SpellEffect.kEffectTarget.kFriendlyTeamAllAtOnce:
+                targets = _activeSubCircles.Where(x => x.OccupiedTeam == caster.OccupiedTeam).ToArray();
+                break;
+            case SpellEffect.kEffectTarget.kEnemyTeam:
+            case SpellEffect.kEffectTarget.kEnemyTeamAllAtOnce:
+                targets = _activeSubCircles.Where(x => x.OccupiedTeam != caster.OccupiedTeam).ToArray();
+                break;
+        }
+
+        return targets;
+    }
+}
