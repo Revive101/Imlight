7d0e3e24cae92435055947d3dbd6877cc4bc0b88
Jay <compundbread@gmail.com>
2024-06-03 17:02:12 -0400

    ref: `WizardZone` cleanup`

--- src/CoreLib/Game/Zone/WizardZone.cs
+++ src/CoreLib/Game/Zone/WizardZone.cs
@@ -19,6 +19,7 @@ using Imlight.CoreLib.WizardData.Models.Player;
 using Imlight.CoreLib.Shared.Resources;
 using Imlight.CoreLib.Shared.Character;
 using static Imlight.Common.Caches.TypeCache;
+using Imlight.Common.IO;
 
 namespace Imlight.CoreLib.Game.Zone;
 
@@ -29,22 +30,31 @@ public class WizardZone : ReceiveProtocolDispatcher, IWithTimers {
     private const ushort RESERVED_MOBILE_ID_MAX = 1000;
     private const uint HEAL_INTERVAL_PER_MINUTE_IN_SECONDS = 5;
 
+    private static readonly Vector4 s_locationFailedGiveaway = new(float.MaxValue, float.MaxValue, float.MaxValue, float.MaxValue);
+
     public string ZoneName { get; }
-    public string ZoneDisplayName { get; set; }
+    public string ZoneDisplayName { get {
+        if (ZoneData is null) {
+            return ZoneName;
+        }
+
+        return ZoneData.m_zoneDisplayName;
+    }}
     public WizZoneData ZoneData { get; set; }
     public ITimerScheduler Timers { get; set; }
 
-    private readonly CoreObjectSerializer _coreObjectSerializer = new CoreObjectSerializer()
+    private readonly CoreObjectSerializer _zoneObjectSerializer = new CoreObjectSerializer()
             .OnBehaviors(SerializerOptions.Behaviors.None)
             .OnPropertyMask(SerializerOptions.PropertyFlags.Public
                 | SerializerOptions.PropertyFlags.Transmit
                 | SerializerOptions.PropertyFlags.AuthorityTransmit);
     private readonly uint _dynamicZoneId;
-    private readonly IActorRef _objectSupervisorRef;
-    private readonly IActorRef _sigilSupervisorRef;
-    private readonly IActorRef _duelSupervisorRef;
+    private readonly IActorRef _objectSupervisorRef; // Supervisor for all objects in this zone.
+    private readonly IActorRef _sigilSupervisorRef;  // Supervisor for all sigils in this zone.
+    private readonly IActorRef _duelSupervisorRef;   // Supervisor for all duels in this zone.
     private readonly Dictionary<IActorRef, Wizard> _zonePlayers;
-    private ushort _zoneObjectMobileIdCounter;
+    private readonly string _mobileIdLock = string.Empty;
+    private ushort _reservedMobileIdCounter;
 
     // ctor
     public WizardZone(string zoneName) {
@@ -106,7 +116,7 @@ public class WizardZone : ReceiveProtocolDispatcher, IWithTimers {
     }
 
     private void BroadcastObjectCreation(CoreObject obj)
-        => Broadcast(new GAME_5_PROTOCOL.MSG_NEWOBJECT { Data = _coreObjectSerializer.Serialize(obj) });
+        => Broadcast(new GAME_5_PROTOCOL.MSG_NEWOBJECT { Data = _zoneObjectSerializer.Serialize(obj) });
 
     private void InformZoneObjectsOfJoin(ZONE_102_PROTOCOL.MSG_ADDPLAYER message) {
         var msg = new ZONE_102_PROTOCOL.MSG_ZONEOBJECTBROADCAST {
@@ -117,6 +127,7 @@ public class WizardZone : ReceiveProtocolDispatcher, IWithTimers {
         // Forward the new player message to every zone object so that they may personally deal with this situation.
         _objectSupervisorRef.Tell(msg);
         _sigilSupervisorRef.Tell(msg);
+        _duelSupervisorRef.Tell(msg);
 
         // Broadcast this new player to each existing player in the zone.
         BroadcastObjectCreation(message.PlayerObject);
@@ -133,44 +144,51 @@ public class WizardZone : ReceiveProtocolDispatcher, IWithTimers {
         // Now spawn each existing player.
         foreach (var obj in _zonePlayers.Values) {
             var playerObj = WizardObjectLoader.GetPlayerGameObject(obj);
-            var msg = new GAME_5_PROTOCOL.MSG_NEWOBJECT { Data = _coreObjectSerializer.Serialize(playerObj) };
+            var msg = new GAME_5_PROTOCOL.MSG_NEWOBJECT { Data = _zoneObjectSerializer.Serialize(playerObj) };
             client.Tell(msg);
         }
     }
 
     private static uint GenerateDynamicZoneId() {
+        // This could be done on the server rather than the zone, but the chances of collision
+        // are so low that it's not worth the extra work.
         var random = new Random();
         return (uint) random.Next(0, int.MaxValue);
     }
 
     private ushort GenerateMobileId() {
-        // Avoid collisions as much as possible.
-        ushort test;
-        var r = new Random();
-        while (true) {
-            test = (ushort) r.Next(RESERVED_MOBILE_ID_MAX, ushort.MaxValue);
-            if (_zonePlayers.Values.Any(x => x.GameObject.m_nMobileID == test)) {
-                continue;
+        lock (_mobileIdLock) {
+            // Avoid collisions as much as possible.
+            ushort test;
+            var r = new Random();
+            while (true) {
+                test = (ushort) r.Next(RESERVED_MOBILE_ID_MAX, ushort.MaxValue);
+                if (_zonePlayers.Values.Any(x => x.GameObject.m_nMobileID == test)) {
+                    continue;
+                }
+
+                break;
             }
 
-            break;
+            return test;
         }
-
-        return test;
     }
 
     private ushort GenerateReservedMobileId() {
-        if (_zoneObjectMobileIdCounter + 1 >= RESERVED_MOBILE_ID_MAX) {
+        if (_reservedMobileIdCounter + 1 >= RESERVED_MOBILE_ID_MAX) {
             throw new Exception($"Zone \"{ZoneName}\" reached the maximum reserved mobile ID count!");
         }
 
-        return ++_zoneObjectMobileIdCounter;
+        return ++_reservedMobileIdCounter;
     }
 
     #region Handlers
 
     [MessageHandler(typeof(ZONE_102_PROTOCOL.MSG_ZONETRANSFER))]
     private void ReceiveZoneTransfer(ZONE_102_PROTOCOL.MSG_ZONETRANSFER message) {
+        // Received when a player wants to transfer into this zone.
+        // All we want to do is return details about the zone and the actual coordinates of where they want to be located.
+        // If the actor accepts, they'll send a ZONE_102_PROTOCOL.MSG_ADDPLAYER message.
         var rsp = new ZONE_102_PROTOCOL.MSG_ZONETRANSFERRSP {
             ZoneActorRef = Self,
             DynamicZoneId = _dynamicZoneId,
@@ -179,37 +197,19 @@ public class WizardZone : ReceiveProtocolDispatcher, IWithTimers {
             ZoneDisplayName = ZoneDisplayName
         };
 
-        // The location given is a ByteString. If it's coordinates, then we need to parse it.
-        // Otherwise, it's a location within this zone. Search through locations to see if we can find it.
-        var actualLocation = Vector3.Zero;
-        var actualOrientation = 0.0f;
+        var actualLocation = GetLocationFromString(message.DestinationLocation);
+        if (actualLocation == s_locationFailedGiveaway) {
+            // We weren't able to find the location. This is an error.
+            Logger.Error("Zone {ZoneName} tried to transfer to an unknown location: {Location}",
+                Logger.Args(ZoneName, message.DestinationLocation));
 
-        var location = Util.GetVectorFromCompactString(message.DestinationLocation);
-        if (location.X != 0 || location.Y != 0 || location.Z != 0) {
-            actualLocation = new Vector3(location.X, location.Y, location.Z);
-            actualOrientation = location.W;
-        }
-        else {
-            // We need to find the location in this zone.
-            var searchedLoc = ZoneData.m_locationList.FirstOrDefault(x => x.m_locName == message.DestinationLocation);
-            if (searchedLoc is null) {
-                // We weren't able to find the location. This is an error.
-                Logger.Error("Zone {ZoneName} tried to transfer to an unknown location: {Location}",
-                    Logger.Args(ZoneName, message.DestinationLocation));
-
-                rsp.ErrorCode = 1;
-            }
-            else {
-                // We found the location. Set the actual location and orientation.
-                var locPos = searchedLoc.m_location;
-                var locOri = searchedLoc.m_direction;
-                actualLocation = locPos;
-                actualOrientation = locOri;
-            }
+            rsp.ErrorCode = 1;
+            Sender.Tell(rsp);
+            return;
         }
 
-        rsp.Location = actualLocation;
-        rsp.Orientation = actualOrientation;
+        rsp.Location = (Vector3) actualLocation;
+        rsp.Orientation = actualLocation.W;
         Sender.Tell(rsp);
     }
 
@@ -217,11 +217,6 @@ public class WizardZone : ReceiveProtocolDispatcher, IWithTimers {
     private void ReceiveAddPlayer(ZONE_102_PROTOCOL.MSG_ADDPLAYER message) {
         if (_zonePlayers.ContainsKey(message.Player)) {
             throw new Exception("Player actor already exists in this zone!");
-        }
-
-        // If the message did not provide a mobile ID, then we need to generate one.
-        if (message.PlayerObject.m_nMobileID == 0) {
-            message.PlayerObject.m_nMobileID = GenerateMobileId();
         }
 
         InformZoneObjectsOfJoin(message);
@@ -232,7 +227,9 @@ public class WizardZone : ReceiveProtocolDispatcher, IWithTimers {
 
         // Inform the player that they've been successfully added to the zone. We want to reply to the callee
         // and any services that may be waiting for this reply.
-        var response = new ZONE_102_PROTOCOL.MSG_ADDPLAYERRSP { WizardGameObject = message.PlayerObject };
+        var response = new ZONE_102_PROTOCOL.MSG_ADDPLAYERRSP {
+            GivenMobileId = GenerateMobileId()
+        };
         Sender.Tell(response);
         message.Player.Tell(response);
 
@@ -243,7 +240,7 @@ public class WizardZone : ReceiveProtocolDispatcher, IWithTimers {
     private void ReceiveRemovePlayer(ZONE_102_PROTOCOL.MSG_REMOVEPLAYER message) {
         // Make sure this player is in this zone. This is just a sanity check, and with different race conditions
         // it's possible that this player is no longer in this zone. If that's the case, we just ignore this message.
-        if (!_zonePlayers.TryGetValue(message.Player, out var obj)) {
+        if (!_zonePlayers.Remove(message.Player)) {
             return;
         }
 
@@ -254,8 +251,6 @@ public class WizardZone : ReceiveProtocolDispatcher, IWithTimers {
 
         // Inform every other player that this object has been removed.
         Broadcast(new GAME_5_PROTOCOL.MSG_REMOVEOBJECT { GameObjectID = message.GlobalId });
-
-        _zonePlayers.Remove(message.Player);
 
         var rsp = new ZONE_102_PROTOCOL.MSG_REMOVEPLAYERRSP();
         Sender.Tell(rsp);
@@ -405,4 +400,28 @@ public class WizardZone : ReceiveProtocolDispatcher, IWithTimers {
     }
 
     #endregion
+
+    private Vector4 GetLocationFromString(ByteString location) {
+        var actualLocation = Vector4.Zero;
+
+        // Try to parse the location as a vector: does it have a non-zero value?
+        // Otherwise, we need to find the location in this zone.
+        var parsedLoc = Util.GetVectorFromCompactString(location);
+        if (parsedLoc.X != 0 || parsedLoc.Y != 0 || parsedLoc.Z != 0) {
+            actualLocation = parsedLoc;
+        }
+        else {
+            var searchedLoc = ZoneData.m_locationList.FirstOrDefault(x => x.m_locName == location);
+            if (searchedLoc is null) {
+                return s_locationFailedGiveaway;
+            }
+            else {
+                // We found the location. Set the actual location and orientation.
+                actualLocation = (Vector4) searchedLoc.m_location;
+                actualLocation.W = searchedLoc.m_direction;
+            }
+        }
+
+        return actualLocation;
+    }
 }
