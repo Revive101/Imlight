b373c1f4c913d888714fe3d46572cf106a45f940
Jay <compundbread@gmail.com>
2025-01-06 05:08:11 -0500

    ref: `ReagentFactory` hold templates instead of ID
    - simplified reagent name cleanup
    - changed `s_reagentTemplateIDs` to hold serialized object rather than IDs

--- src/CoreLib/Game/Reagents/ReagentFactory.cs
+++ src/CoreLib/Game/Reagents/ReagentFactory.cs
@@ -3,12 +3,10 @@
  * Proprietary and confidential.
  */
 
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using Imlight.Common;
-using Imlight.Common.Cryptography;
 using Imlight.Common.ObjectProperty;
-using Imlight.Common.ObjectProperty.PropertyReflection;
 using Imlight.CoreLib.Shared.Resources;
 using static Imlight.Common.Caches.TypeCache;
 
@@ -17,7 +15,7 @@ namespace Imlight.CoreLib.Game.Reagents;
 public class ReagentFactory : RootDirectoryResourceSingleton<ReagentFactory>, IMemoryStreamDisposable {
     protected override string DirectoryName => "ObjectData/Reagents/";
 
-    private static readonly Dictionary<string, uint> s_reagentTemplateIDs = new();
+    private static readonly Dictionary<string, ReagentItemTemplate> s_reagentTemplates = [];
 
     protected override void AfterLoad() {
         var serializer = new FileSerializer();
@@ -33,13 +31,11 @@ public class ReagentFactory : RootDirectoryResourceSingleton<ReagentFactory>, IM
                 continue;
             }
 
-            var stringCleaned = fileRecord.FileName // todo: make this better
-                .Replace("ObjectData/Reagents/", "")
-                .Replace("Harvest-", "")
-                .Replace("-01.xml", "")
+            var name = reagentTemplate.m_objectName
+                .ToString()
                 .ToLower();
 
-            s_reagentTemplateIDs.Add(stringCleaned, reagentTemplate.m_templateID);
+            s_reagentTemplates.Add(name, reagentTemplate);
 
             counter++;
         }
@@ -54,13 +50,13 @@ public class ReagentFactory : RootDirectoryResourceSingleton<ReagentFactory>, IM
     /// <returns>The reagent with the specified name, or null if the reagent template is not found.</returns>
     public static ClientReagentItem GetReagent(string reagentName) {
         reagentName = reagentName.ToLower();    
-        foreach (var kp in s_reagentTemplateIDs) {
+        foreach (var kp in s_reagentTemplates) {
             if (kp.Key.Contains(reagentName)) {
-                return GetReagent(kp.Value);
+                return GetReagent(kp.Value.m_templateID);
             }
         }
 
-        Logger.Warning("Could not find reagent template ID by name {0}.", Logger.Args(reagentName));
+        Logger.Warning("Could not find reagent template by name {0}.", Logger.Args(reagentName));
         return null;
     }
 
@@ -70,15 +66,15 @@ public class ReagentFactory : RootDirectoryResourceSingleton<ReagentFactory>, IM
     /// <param name="templateId">The ID of the reagent template.</param>
     /// <returns>The created reagent object.</returns>
     public static ClientReagentItem GetReagent(uint templateId) {
-        var template = CoreObjectFactory.GetCoreTemplate(templateId);
+        var templateFound = s_reagentTemplates.FirstOrDefault(x => x.Value.m_templateID == templateId).Value;
 
-        if (template is null) {
-            Logger.Warning("Could not find reagent template with ID {0}.", Logger.Args(templateId));
+        if (templateFound is null) {
+            Logger.Warning("Could not find reagent template ID {0}.", Logger.Args(templateId));
             return null;
         }
 
-        if (template is not ReagentItemTemplate reagentTemplate) {
-            Logger.Warning("Template with ID {0} is not a reagent template.", Logger.Args(templateId));
+        if (templateFound is not ReagentItemTemplate) {
+            Logger.Warning("Could not find reagent template ID {0}.", Logger.Args(templateId));
             return null;
         }
 
@@ -86,6 +82,6 @@ public class ReagentFactory : RootDirectoryResourceSingleton<ReagentFactory>, IM
     }
 
     public void DisposeStream() {
-        s_reagentTemplateIDs.Clear();
+        s_reagentTemplates.Clear();
     }
 }
