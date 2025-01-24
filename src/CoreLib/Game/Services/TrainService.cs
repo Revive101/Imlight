3adb2b03c773c3c0d997b6de823201e71f0f23f5
Jay <compundbread@gmail.com>
2025-01-24 10:21:17 -0500

    feat: re-add trainers

--- src/CoreLib/Game/Services/TrainService.cs
+++ src/CoreLib/Game/Services/TrainService.cs
@@ -8,12 +8,15 @@ using Imlight.Common.Caches;
 using Imlight.CoreLib.Shared.Networking;
 using Imlight.CoreLib.Shared.Packets;
 using Imlight.Common;
+using Imlight.CoreLib.Game.Zone.Components;
+using Imlight.CoreLib.Shared.Resources;
+using static Imlight.Common.Caches.TypeCache;
+using Imlight.CoreLib.Game.Spells;
 
 namespace Imlight.CoreLib.Game.Services;
 
-internal class TrainService : MessageService {
-    public TrainService(SessionActor sessionActor) : base(sessionActor) { }
-
+internal class TrainService(SessionActor sessionActor) : MessageService(sessionActor) {
+    
     protected static Props Props(SessionActor parentActor)
         => Akka.Actor.Props.Create(() => new TrainService(parentActor));
 
@@ -28,26 +31,51 @@ internal class TrainService : MessageService {
             var wizardName = GetActiveWizard().PlayerNameBehavior.GetWizardName();
             Logger.Error("{0} searched for training NPC {1} (mobile ID), but it was not found within the zone.",
                 Logger.Args(wizardName, message.MobileID));
+
             return;
         }
 
-        // todo: fixme
-        /*
+        // Ensure that the interacted object has a TrainerComponent.
+        var trainerComponent = response.ZoneObject.GetComponentOfType<TrainerComponent>();
+        if (trainerComponent is null) {
+            Logger.Error("NPC {0} does not have a TrainerComponent.", Logger.Args(response.ZoneObject.ActiveGameObject.m_debugName));
+
+            return;
+        }
+
+        // We don't need to inform the TrainerComponent of our interaction, as the client
+        // doesn't even send an interaction message upon training.
         var wizard = GetActiveWizard();
-        var npcObj = (WizardZoneNpc) response.ZoneObject;
 
-        var serviceOption = (ServiceOptionTrain) npcObj.ServiceOptions.FirstOrDefault(x => x.ServiceName == "WizTrainingService");
-        if (serviceOption is null) {
-            Logger.Error("NPC {0} does not have a WizTrainingService option.", Logger.Args(npcObj.ActiveGameObject.m_debugName));
+        // Ensure that the training index is within the bounds of the spell inventory.
+        if (message.TrainingIndex < 0 || message.TrainingIndex >= trainerComponent.SpellInventory.Count) {
+            Logger.Error("Wizard {0} attempted to train spell at index {1}, but it is out of bounds.",
+                Logger.Args(wizard.PlayerNameBehavior.GetWizardName(), message.TrainingIndex));
+
             return;
         }
 
-        var spellEntry = serviceOption.SpellInventory[message.TrainingIndex];
+        var spellEntry = trainerComponent.SpellInventory[message.TrainingIndex];
         var spellTemplate = (SpellTemplate) CoreObjectFactory.GetCoreTemplate(spellEntry.TemplateID);
+
+        // Determine if the wizard has enough training points to learn the spell.
+        // Wizards that are of the same magic school as the spell they are training have a cost of 0.
         var spellCost = wizard.MagicSchoolBehavior.MagicSchool.ToString() == spellTemplate.m_sMagicSchoolName ? 0 : 1;
+        if (wizard.MagicSchoolBehavior.TrainingPoints < spellCost) {
+            Logger.Error("Wizard {0} attempted to train spell {1} but does not have enough training points.",
+                Logger.Args(wizard.PlayerNameBehavior.GetWizardName(), spellEntry.TemplateID));
+
+            return;
+        }
 
         var spell = SpellFactory.GetSpell((uint) spellEntry.TemplateID);
-        wizard.LearnSpell(spell);
+        var spellLearnedSuccess = wizard.LearnSpell(spell);
+        if (!spellLearnedSuccess) {
+            Logger.Error("Wizard {0} attempted to train spell {1} but failed to learn it.",
+                Logger.Args(wizard.PlayerNameBehavior.GetWizardName(), spellEntry.TemplateID));
+
+            return;
+        }
 
         var addSpellMsg = new WIZARD_12_PROTOCOL.MSG_ADDSPELLTOBOOK() {
             SpellID = (int) spellEntry.TemplateID
@@ -68,6 +96,6 @@ internal class TrainService : MessageService {
             Success = 1
         };
         SendToSocket(trainCompleteMsg);
-        */
     }
+
 }
