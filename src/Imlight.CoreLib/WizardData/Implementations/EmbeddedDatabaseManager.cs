467e8d659f9aa740de76d234379d8ecc3b02f998
Jay <compundbread@gmail.com>
2025-03-20 23:07:33 -0400

    chore: better logs for embedded database

--- src/Imlight.CoreLib/WizardData/Implementations/EmbeddedDatabaseManager.cs
+++ src/Imlight.CoreLib/WizardData/Implementations/EmbeddedDatabaseManager.cs
@@ -15,6 +15,17 @@ public static class EmbeddedDatabaseManager {
 
     public static bool IsRunning { get; private set; }
 
+    private static readonly string s_embeddedDatabasePath 
+        = ConfigurationManager.Settings["Database.EmbeddedDatabaseDataDirectory"];
+    private static readonly bool s_embeddedDatabaseUseFull 
+        = ConfigurationManager.Settings["Database.EmbeddedDatabaseUseFull"].AsBool();
+    private static readonly string s_embeddedDatabaseFullPath 
+        = ConfigurationManager.Settings["Database.EmbeddedDatabaseFullPath"];
+    private static readonly ushort s_embeddedDatabasePort 
+        = ConfigurationManager.Settings["Database.EmbeddedDatabasePort"].AsUShort();
+    private static readonly long s_embeddedDatabaseTimeoutTime 
+        = ConfigurationManager.Settings["Database.EmbeddedDatabaseTimeoutTime"].AsLong();
+
     public static void Start() {
         try {
             if (IsRunning) {
@@ -22,21 +33,23 @@ public static class EmbeddedDatabaseManager {
             }
 
             Logger.Information("Initializing embedded RavenDB database for the first time..");
+            Logger.Information("Database data directory: {0}", Logger.Args(s_embeddedDatabasePath));
 
             // Configure the embedded server.
             var serverOptions = new ServerOptions {
-                DataDirectory = ConfigurationManager.Settings["Database.EmbeddedDatabaseDataDirectory"],
-                ServerUrl = $"http://127.0.0.1:{ConfigurationManager.Settings["Database.EmbeddedDatabasePort"]}",
-                MaxServerStartupTimeDuration =
-                    TimeSpan.FromSeconds(ConfigurationManager.Settings["Database.EmbeddedDatabaseTimeoutTime"].AsLong()),
+                DataDirectory = s_embeddedDatabasePath,
+                ServerUrl = $"http://127.0.0.1:{s_embeddedDatabasePort}",
+                MaxServerStartupTimeDuration = TimeSpan.FromSeconds(s_embeddedDatabaseTimeoutTime),
                 CommandLineArgs = ["--Databases.MaxIdleTimeInSec=-1"]
             };
 
             // If we're using the full database, we need to set the server directory.
             // Otherwise, we're using the embedded database.
-            var useFullDb = ConfigurationManager.Settings["Database.EmbeddedDatabaseUseFull"].AsBool();
-            if (useFullDb) {
-                serverOptions.ServerDirectory = ConfigurationManager.Settings["Database.EmbeddedDatabaseFullPath"];
+            if (s_embeddedDatabaseUseFull) {
+                serverOptions.ServerDirectory = s_embeddedDatabaseFullPath;
+
+                Logger.Information("Using full RavenDB server at {0}", 
+                    Logger.Args(s_embeddedDatabaseFullPath));
             }
 
             EmbeddedServer.Instance.StartServer(serverOptions);
@@ -44,7 +57,14 @@ public static class EmbeddedDatabaseManager {
 
             EmbeddedServer.Instance.ServerProcessExited += (sender, args) => {
                 Logger.Error("Embedded database process exited unexpectedly. Restarting..");
-                EmbeddedServer.Instance.RestartServerAsync().Wait();
+                
+                try {
+                    EmbeddedServer.Instance.RestartServerAsync().Wait();
+                }
+                catch (Exception ex) {
+                    Logger.Error("Failed to restart embedded database: {0}", Logger.Args(ex.Message));
+                }
+
                 Logger.Information("Embedded database restarted.");
             };
 
