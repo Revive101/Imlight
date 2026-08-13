5602e1e1f5a4d20f0611e08267d98ff34a37194b
Jay <jay@ctrldevil.com>
2026-08-13 19:54:27 -0400

    `Feat/mob drops` -> `quality-assurance` (#155)
    * fix: exclusive zone loader
    
    * feat: `IHandshakeService`
    
    Co-Authored-By: kohdra32 <294984461+kohdra32@users.noreply.github.com>
    
    * fix: treasure card draws
    
    Co-Authored-By: kohdra32 <294984461+kohdra32@users.noreply.github.com>
    
    * fix: proper sync of combat participant index
    
    Co-Authored-By: kohdra32 <294984461+kohdra32@users.noreply.github.com>
    
    * fix: reagent pickups
    
    Co-Authored-By: kohdra32 <294984461+kohdra32@users.noreply.github.com>
    
    * chore: comments
    
    * fix: no mounts in certain zones
    
    * fix: x-pip spell selection
    
    * feat: add combat minions
    
    * chore: code sanity
    
    * fix: hate table bad math
    
    * fix: default decks for creatures
    
    * feat: minion targets
    
    * feat: `kKillCreature` & `kInstantKill`
    
    * feat: `kModifyPips`
    
    * fix: minion health
    
    * fix: combat turn sigil
    
    * fix: minion owner
    
    * feat: `kMaxHealthDamage`
    
    * feat: add `NpcDropTableCollection` & `NpcDropTable`
    
    * docs: update with combat rewards
    
    * ref: migrate -> `LootGranter`
    
    * fix: actually add droptablecollection
    
    * feat: combat loot
    
    * chore: comment sanity
    
    * fix: targeting
    
    ---------
    
    Co-authored-by: kohdra32 <294984461+kohdra32@users.noreply.github.com>

--- src/Imlight.CoreLib/Game/World/GameWorld.cs
+++ src/Imlight.CoreLib/Game/World/GameWorld.cs
@@ -83,6 +83,7 @@ public class GameWorld : ReceiveProtocolDispatcher, IWithTimers {
         NpcInventoryCollection.PreloadInventories();
         NpcSpellInventoryCollection.PreloadNpcSpellInventories();
         CreatureSpellbookCollection.PreloadSpellbooks();
+        NpcDropTableCollection.PreloadDropTables();
     }
 
     public static Props Props(GameServer server)
